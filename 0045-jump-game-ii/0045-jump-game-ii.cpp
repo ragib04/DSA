e3a58279e58fe5ee511ae15fccf4c56ac35a33ac
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-07 20:49:36 +0530

    Time: 0 ms (100%), Space: 20.4 MB (62.35%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,7 +5,7 @@ public:
         int near = 0, far = 0, jump = 0;
         while(far < n-1){
             int farthest = 0;
-            for(int  i = 0; i<=far; i++){
+            for(int  i = near; i<=far; i++){
                 farthest = max(farthest, i+nums[i]);
             }
             near = far+1;
