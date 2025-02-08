15b1066ad6b9ecaa955ac3e4ab4328286aebe85e
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-09 00:12:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+// Program to find the maximum profit job sequence from a given array
+// of jobs with deadlines and profits
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> JobSequencing(vector<int> &id, vector<int> &deadline,
+                              vector<int> &profit) {
+        // code here
+        int n = deadline.size();
+        int cnt = 0;
+         int p = 0;
+         priority_queue<int, vector<int>, greater<int>> pq;
+         vector<int> ans;
+         vector<pair<int, int>> v;
+         for(int i =0; i<n; i++){
+             v.push_back({deadline[i], profit[i]});
+             
+         }
+         sort(v.begin(), v.end());
+         for(auto it:v){
+             int d= it.first;
+             int wt = it.second;
+             
+             if(d>cnt){
+                 p += wt;
+                 pq.push(wt);
+                 cnt++;
+                 
+             }
+             else{
+                 if(d==cnt){
+                     if(!pq.empty() && wt>pq.top()){
+                         p -= pq.top();
+                         pq.pop();
+                         p += wt;
+                         pq.push(wt);
+                     }
+                 }
+             }
+         }
+             ans.push_back(cnt);
+             ans.push_back(p);
+             return ans;
+             
+         
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+//            Driver program to test methods
+int main() {
+    int t;
+    // testcases
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> jobIDs, deadlines, profits;
+        string temp;
+        getline(cin, temp);
+        istringstream ss1(temp);
+        int x;
+        while (ss1 >> x)
+            jobIDs.push_back(x);
+
+        getline(cin, temp);
+        istringstream ss2(temp);
+        while (ss2 >> x)
+            deadlines.push_back(x);
+
+        getline(cin, temp);
+        istringstream ss3(temp);
+        while (ss3 >> x)
+            profits.push_back(x);
+
+        int n = jobIDs.size();
+
+        Solution obj;
+        vector<int> ans = obj.JobSequencing(jobIDs, deadlines, profits);
+        cout << ans[0] << " " << ans[1] << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
