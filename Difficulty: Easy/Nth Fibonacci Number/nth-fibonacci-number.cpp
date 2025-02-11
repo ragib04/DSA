c7a410a97e2c052670d9913df25441a423809971
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-12 00:46:54 +0530

    Update nth-fibonacci-number.cpp

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -10,7 +10,7 @@ class Solution {
   public:
  
  
-
+// recursive------->
     int nthFibonacci(int n) {
         // code here
         vector<int> dp(n+1);
@@ -23,6 +23,28 @@ class Solution {
         
         
     }
+
+
+  //dp-------->
+  int fib(int n, vector<int> &dp){
+      //base case
+      if(n==0 || n==1) return n;
+      //step 3 check memorize vale
+      if(dp[n] != -1) return dp[n];
+      //step 2 store prev value
+      dp[n] = fib(n-1, dp)+ fib(n-2, dp);
+      return dp[n];
+  }
+    int nthFibonacci(int n) {
+        // code here
+        //step 1 memorize
+        vector<int> dp(n+1,-1);
+        return fib(n, dp);
+        
+        
+    }
+
+
 };
 
 //{ Driver Code Starts.
