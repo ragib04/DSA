ce3283beaa1bdbda782568795677719de760f0de
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-13 02:04:35 +0530

    Time: 3 ms (67.95%), Space: 7.9 MB (34.22%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -14,14 +14,19 @@ public:
 
     int fib(int n) {
 
-        vector<int> dp(n + 1);
-        if(n==0) return 0;
+       
+       int prev1 = 1;
+       int prev2 = 0;
+        if(n==0) return prev2;
 
-        dp[1] = 1;
-
+       
+int curr;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+
         }
-        return dp[n];
+        return curr;
     }
 };
