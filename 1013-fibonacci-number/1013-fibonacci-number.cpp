53eb5b82cd14f81357ba840e0f6488388dd66c01
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-13 02:05:09 +0530

    Time: 2 ms (78.83%), Space: 7.6 MB (86.75%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -20,13 +20,13 @@ public:
         if(n==0) return prev2;
 
        
-int curr;
+
         for (int i = 2; i <= n; i++) {
-            curr = prev1+prev2;
+            int curr = prev1+prev2;
             prev2 = prev1;
             prev1 = curr;
 
         }
-        return curr;
+        return prev1;
     }
 };
