11b056625943414c4c9fd6f871c64a0a926a19dc
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-19 23:41:10 +0530

    Time: 3 ms (74.09%), Space: 7.9 MB (90.21%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
 
-void solve(int n, string &path, vector<string> &res){
+void solve(int n, string &path, string &res, int k, int &cnt){
     //base case
     if(path.length() == n){
-        res.push_back(path);
+        cnt++;
+        if(cnt == k){
+            res = path;
+        
+        }
+        
         return;
     }
     for(char ch = 'a'; ch<='c'; ch++){
@@ -12,17 +17,16 @@ void solve(int n, string &path, vector<string> &res){
         //do
         path.push_back(ch);
         //explore
-        solve(n, path, res);
+        solve(n, path, res, k, cnt);
         //undo
         path.pop_back();
     }
 }
     string getHappyString(int n, int k) {
         string path = "";
-        vector<string> res;
-        solve(n, path, res);
-        if(res.size()<k) return "";
-
-        return res[k-1];
+        string res = "";
+        int cnt = 0;
+        solve(n, path, res, k, cnt);
+        return res;
     }
 };
