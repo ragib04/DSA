8833820ecbe69ccd8fcd5fcba9fedaab96f1de0d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-26 03:18:57 +0530

    Time: 0 ms (100%), Space: 10 MB (89.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -37,16 +37,19 @@ int hrob(int ind, vector<int> &nums){
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n);
-        dp[0] = nums[0];
+        //vector<int> dp(n);
+        int prev1 = nums[0];
+        int prev2 = 0;
        
         for(int i = 1; i<n; i++){
             int pick = nums[i];
-            if(i>1) pick += dp[i-2];
-            int notpick = dp[i-1];
+            if(i>1) pick += prev2;
+            int notpick = 0+prev1;
 
-            dp[i] = max(pick, notpick);
+            int curr = max(pick, notpick);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
