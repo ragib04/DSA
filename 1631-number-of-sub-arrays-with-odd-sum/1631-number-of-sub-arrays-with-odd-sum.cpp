db40e9806f87aa36058cfd70af66919a3c705e39
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-26 00:56:19 +0530

    Time: 4 ms (62.15%), Space: 112 MB (80%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int numOfSubarrays(vector<int>& arr) {
-        const int MOD = 1e9+7;
-        int oddcnt = 0, evencnt = 1;
-        int sum = 0, ans = 0;
-        for(int num: arr){
-            sum += num;
+int MOD = 1e9+7;
 
-            if(sum%2 == 0){
-                ans = (ans+oddcnt)%MOD;
-                evencnt++;
-            }
-            else{
-                ans = (ans+evencnt)%MOD;
-                oddcnt++;
-            }
+    int numOfSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        int oddcnt = 0, evencnt = 1;
+    int sum = 0, cnt = 0;
+
+    for(int i = 0; i<n; i++){
+        sum += arr[i];
+        if(sum%2 == 0){
+            cnt = (cnt+oddcnt)%MOD;
+            evencnt++;
         }
-        return ans;
+        else{
+            cnt = (cnt+evencnt)%MOD;
+            oddcnt++;
+        }
+    }
+    return cnt;
     }
 };
