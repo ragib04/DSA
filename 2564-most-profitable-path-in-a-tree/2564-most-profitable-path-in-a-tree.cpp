ff459bdb77c30dc1556b82f8504bfa77662add87
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-25 15:31:28 +0530

    Time: 193 ms (43.64%), Space: 173.1 MB (32.84%) - LeetHub

--- 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-unordered_map<int, vector<int>> adj;
+unordered_map<int, vector<int>>adj; //adjlist
 unordered_map<int, int> bobMap;
 int aliceIncome;
+bool DFSbob(int bob, int t, vector<bool> &visited){
+    visited[bob] = true;
+    bobMap[bob] = t;
 
-bool DFSbob(int curr, int t, vector<bool> &visited){
-    visited[curr] = true;
-    bobMap[curr] = t;
+    if(bob == 0) return true;
 
-    if(curr == 0){
-        return true;
-
-    }
-    for(auto &ngbr: adj[curr]){
+    for(auto &ngbr: adj[bob]){
         if(!visited[ngbr]){
-            if(DFSbob(ngbr, t+1, visited) == true){
-                return true;
-            }
+            if(DFSbob(ngbr, t+1, visited) == true) return true;
         }
     }
-    bobMap.erase(curr);
+    bobMap.erase(bob);
     return false;
 }
+
 void DFSAlice(int curr, int t, int income, vector<bool> &visited, vector<int> &amount){
     visited[curr] = true;
-    if(bobMap.find(curr) == bobMap.end() || t< bobMap[curr]){
+    if(bobMap.find(curr) == bobMap.end() || t < bobMap[curr]){
         income += amount[curr];
     }
-    else if(t== bobMap[curr]){
+    else if(t == bobMap[curr]){
         income += (amount[curr]/2);
     }
-    if(adj[curr].size() == 1 && curr != 0){
+
+    if(curr != 0 && adj[curr].size() == 1){
         aliceIncome = max(aliceIncome, income);
     }
-    for(int &ngbr : adj[curr]){
+
+    for(int &ngbr: adj[curr]){
         if(!visited[ngbr]){
             DFSAlice(ngbr, t+1, income, visited, amount);
         }
@@ -43,20 +41,22 @@ void DFSAlice(int curr, int t, int income, vector<bool> &visited, vector<int> &a
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
         int n = amount.size();
         aliceIncome = INT_MIN;
-        for(vector<int> &edge : edges){
+        for(vector<int> &edge: edges){
             int u = edge[0];
             int v = edge[1];
 
             adj[u].push_back(v);
             adj[v].push_back(u);
+
         }
-        int time = 0; vector<bool> visited(n, false);
+        int time = 0;
+        vector<bool> visited(n,false);
         DFSbob(bob, time, visited);
 
         int income = 0;
         visited.assign(n, false);
         DFSAlice(0, 0, income, visited, amount);
-
         return aliceIncome;
+        
     }
-}; 
+};
