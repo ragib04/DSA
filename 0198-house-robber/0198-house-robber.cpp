72cfed987f4944828207fe088f0feaaa9652fb71
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-02-26 03:04:43 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,8 +15,8 @@ int hrob(int ind, vector<int> &nums){
         int n = nums.size();
         return hrob(n-1, nums);
     }*/
-
-    // Tabulation
+/*
+    // memoization
 
     int func(int ind, vector<int> &nums, vector<int> &dp){
         if(ind<0) return 0;
@@ -32,5 +32,21 @@ int hrob(int ind, vector<int> &nums){
         int n = nums.size();
         vector<int> dp(n, -1);
         return func(n-1, nums, dp);
+    }*/
+
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+       
+        for(int i = 1; i<n; i++){
+            int pick = nums[i];
+            if(i>1) pick += dp[i-2];
+            int notpick = dp[i-1];
+
+            dp[i] = max(pick, notpick);
+        }
+        return dp[n-1];
     }
 };
