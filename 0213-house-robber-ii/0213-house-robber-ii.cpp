28af148f7720d71600f947f0a49323a71e052e7a
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-01 00:34:49 +0530

    Time: 0 ms (100%), Space: 10.3 MB (66.43%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+int robber(vector<int> &nums){
+    int n = nums.size();
+    
+    int prev1 = nums[0], prev2 = 0;
+    for(int i = 1; i<n; i++){
+        int take = nums[i];
+        if(i>1) take += prev2;
+        int nottake = prev1;
+        int curr = max(take, nottake);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+return prev1;
+
+}
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> temp1, temp2;
+        for(int i = 0; i<n; i++){
+            if(i != 0) temp1.push_back(nums[i]);    //ignore the first idx
+            if(i != n-1) temp2.push_back(nums[i]);  //ignore the last idx
+        }
+        return max(robber(temp1), robber(temp2));
+
+    }
+};
