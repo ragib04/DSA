ce9ac902a82278c37f62d78b2b4cbbc2f05c25c6
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-04 01:14:12 +0530

    Time: 2 ms (90.76%), Space: 131.9 MB (68.95%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,22 +1,52 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> list1, list2, list3;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]<pivot) list1.push_back(nums[i]);
-            else if(nums[i]>pivot) list2.push_back(nums[i]);
-            else list3.push_back(nums[i]);
-        }
-
+        // vector<int> list1, list2, list3;
         // for(int i = 0; i<nums.size(); i++){
-        //     if(nums[i] == pivot) list1.push_back(nums[i]);
+        //     if(nums[i]<pivot) list1.push_back(nums[i]);
+        //     else if(nums[i]>pivot) list2.push_back(nums[i]);
+        //     else list3.push_back(nums[i]);
         // }
-        list1.insert(list1.end(), list3.begin(), list3.end());
-        // for(int i = 0 ; i<list2.size(); i++){
-        //     list1.push_back(list2[i]);
-        // }
-        list1.insert(list1.end(), list2.begin(), list2.end());
-        nums = list1;
-        return nums;
+
+        // // for(int i = 0; i<nums.size(); i++){
+        // //     if(nums[i] == pivot) list1.push_back(nums[i]);
+        // // }
+        // list1.insert(list1.end(), list3.begin(), list3.end());
+        // // for(int i = 0 ; i<list2.size(); i++){
+        // //     list1.push_back(list2[i]);
+        // // }
+        // list1.insert(list1.end(), list2.begin(), list2.end());
+        // nums = list1;
+        // return nums;
+
+
+        //app2->>>>>>>>>
+        int cntless = 0, cntequal = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i]<pivot){
+                cntless++;
+                } 
+            else if(nums[i] == pivot) {
+                cntequal++;
+            }
+        }
+        vector<int> ans(nums.size());
+        int i = 0, j= cntless, k = cntless+cntequal;
+
+        for(int &num : nums){
+            if(num<pivot){
+                ans[i] = num;
+                i++;
+            }
+            else if(num == pivot){
+                ans[j] = num;
+                j++;
+            }
+            else{
+                ans[k] = num;
+                k++;
+            }
+        }
+        return nums = ans;
     }
 };
