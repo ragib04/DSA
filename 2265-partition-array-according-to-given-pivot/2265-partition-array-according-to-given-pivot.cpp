912216df79da83a69118650e319523f9c78bc32f
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-04 00:29:19 +0530

    Time: 13 ms (18.1%), Space: 143.3 MB (7.17%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> list1, list2;
+        vector<int> list1, list2, list3;
         for(int i = 0; i<nums.size(); i++){
             if(nums[i]<pivot) list1.push_back(nums[i]);
             else if(nums[i]>pivot) list2.push_back(nums[i]);
+            else list3.push_back(nums[i]);
         }
 
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i] == pivot) list1.push_back(nums[i]);
-        }
+        // for(int i = 0; i<nums.size(); i++){
+        //     if(nums[i] == pivot) list1.push_back(nums[i]);
+        // }
+        list1.insert(list1.end(), list3.begin(), list3.end());
         // for(int i = 0 ; i<list2.size(); i++){
         //     list1.push_back(list2[i]);
         // }
