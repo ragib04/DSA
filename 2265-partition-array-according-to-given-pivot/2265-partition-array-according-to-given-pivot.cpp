f742c9a87e5f910a14cfc933cc4265875273393f
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-04 00:27:12 +0530

    Time: 2 ms (90.85%), Space: 139.9 MB (14.34%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -10,9 +10,10 @@ public:
         for(int i = 0; i<nums.size(); i++){
             if(nums[i] == pivot) list1.push_back(nums[i]);
         }
-        for(int i = 0 ; i<list2.size(); i++){
-            list1.push_back(list2[i]);
-        }
+        // for(int i = 0 ; i<list2.size(); i++){
+        //     list1.push_back(list2[i]);
+        // }
+        list1.insert(list1.end(), list2.begin(), list2.end());
         nums = list1;
         return nums;
     }
