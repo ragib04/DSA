b3e1c2a2d749a8ec19b36c8e172264aeecda6013
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-06 06:54:34 +0530

    Time: 39 ms (9.56%), Space: 8 MB (13.89%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+//recursive app--------->>>>>>>
+
+int solve(int n, int p){
+    if(n==0) return true;
+    if(pow(3,p)>n) return false;
+
+    bool p_pick = solve(n-pow(3,p), p+1);
+    bool p_notpick = solve(n, p+1);
+
+    return p_pick || p_notpick;
+}
     bool checkPowersOfThree(int n) {
+        return solve(n,0);
+    }
+    /*
+      bool checkPowersOfThree(int n) {
        for(int i = 16; i>=0; i--){
         int power = pow(3,i);
         if(n>power) n-= power;
@@ -8,4 +23,5 @@ public:
        }
         return false;
     }
+    */
 };
