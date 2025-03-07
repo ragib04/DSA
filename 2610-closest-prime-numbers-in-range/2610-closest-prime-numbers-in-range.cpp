425f879050bd8cf8a8c2bc773642409a312d2ca7
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-07 15:50:06 +0530

    Time: 211 ms (43.7%), Space: 23.4 MB (36.85%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+vector<bool> sive(int right){
+    vector<bool> isprime(right+1, true);
+    isprime[0] = false;
+    isprime[1] = false;
+
+    for(int i = 2; i*i<=right; i++){
+        if(isprime[i] == true){
+            for(int j = 2; j*i<=right; j++){
+                isprime[i*j] = false;
+            }
+        }
+    }
+    return isprime;
+}
+    vector<int> closestPrimes(int left, int right) {
+        vector<bool> isprime = sive(right);
+
+        vector<int> primes;
+        for(int num = left; num<= right; num++){
+            if(isprime[num] == true){
+                primes.push_back(num);
+            }
+        }
+        int mindiff = INT_MAX;
+        vector<int> result = {-1, -1};
+        for(int i = 1; i<primes.size(); i++){
+            int diff = primes[i]-primes[i-1];
+            if(diff<mindiff){
+                mindiff = diff;
+                result = {primes[i-1], primes[i]};
+            }
+        }
+        return result;
+    }
+};
