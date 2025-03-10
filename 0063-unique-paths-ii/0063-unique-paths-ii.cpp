705093ce3fb248ecb884b9b96dafa5d7ef22eb17
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-10 07:28:43 +0530

    Time: 0 ms (100%), Space: 12 MB (29%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
 //memoization
@@ -25,4 +26,27 @@ int solve(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
         return solve(grid, n-1, m-1, dp);
     }
 
+};*/
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        dp[0][0] = 1;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j ==0) dp[0][0] = 1;
+                else{
+                   int left = 0, up = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+
+                    dp[i][j] = left+up;
+                }
+
+            }
+        }
+        return dp[n-1][m-1];
+    }
 };
