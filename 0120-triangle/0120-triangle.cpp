f130b20d96cfa3a9998ae43f10ece16f4b38c522
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-12 08:10:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (81.71%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,3 +1,6 @@
+/*
+
+//memoization
 class Solution {
 public:
 
@@ -13,9 +16,22 @@ int f(int i, int j, vector<vector<int>> &triangle, int n, vector<vector<int>> &d
 }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return f(0, 0, triangle, n, dp);
+    }
+};
+
+
+//Tabulation
+class Solution {
+public:
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
         for(int j = 0; j<n; j++){
             dp[n-1][j] = triangle[n-1][j];
+            }
             for(int i = n-2; i>=0; i--){
                 for(int j = i; j>= 0; j--){
                 
@@ -25,7 +41,34 @@ int f(int i, int j, vector<vector<int>> &triangle, int n, vector<vector<int>> &d
                 dp[i][j] = min(down, diag);
                 }
             }
-        }
+        
         return dp[0][0];
+    }
+};
+*/
+
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> front(n,0), curr(n,0);
+
+        for(int j = 0; j<n; j++){
+            front[j] = triangle[n-1][j];
+        }
+            for(int i = n-2; i>=0; i--){
+                
+                for(int j = i; j>= 0; j--){
+                
+                int down = triangle[i][j] +  front[j];
+                int diag = triangle[i][j] +  front[j+1];
+
+                curr[j] = min(down, diag);
+                }
+                front = curr;
+            }
+        
+        return front[0];
     }
 };
