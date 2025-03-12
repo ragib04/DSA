d2f424adcd29275bb7ec0de039d1256d2325e9b1
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-12 07:04:05 +0530

    Time: 0 ms (100%), Space: 13.1 MB (46.12%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -5,11 +5,11 @@ int f(int i, int j, vector<vector<int>> &triangle, int n, vector<vector<int>> &d
     
     if(i == n-1) return triangle[n-1][j];
     if(dp[i][j] != -1) return dp[i][j];
-    int down = INT_MAX, diag = INT_MAX;
-    down = f(i+1, j, triangle, n, dp);
-     diag = f(i+1, j+1, triangle, n, dp);
 
-    return dp[i][j] = triangle[i][j] + min(down, diag);
+   int  down = triangle[i][j] + f(i+1, j, triangle, n, dp);
+    int  diag = triangle[i][j] + f(i+1, j+1, triangle, n, dp);
+
+    return dp[i][j] = min(down, diag);
 }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
