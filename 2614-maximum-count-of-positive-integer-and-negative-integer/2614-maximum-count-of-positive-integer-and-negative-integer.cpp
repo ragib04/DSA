744e73ba029e3d5de301ad8c9cbe2ab925c07e4d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-12 10:42:01 +0530

    Time: 0 ms (100%), Space: 21.5 MB (10.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int poscnt = 0;
-        int negcnt = 0;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]>0) poscnt++;
-            if(nums[i]<0) negcnt++;
-        }
+        int n = nums.size();
+       int neg = binarysearch(nums, 0, n);  //3
+       int pos = n - binarysearch(nums, 1, n) ; //7-5= 2
+       return max(neg, pos);
+    }
 
-        return max(poscnt, negcnt);
+    int binarysearch(vector<int> &nums, int target, int n){
+        int s = 0, e = n-1;
+        int res = n;
+        while(s<=e){
+            int mid = (s+e)/2;// neg -3 1 pos 3 5
+            if(nums[mid] < target){
+                s = mid+1; // 2 pos 4 5
+            }
+            else{
+                res = mid;  // 3pos = 5
+                e = mid-1;// 2 pos - 4
+            }
+        }
+        return res;
     }
 };
