f60a56a7e610111d6a446897ace644f04c1eb120
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-03-20 12:24:08 +0530

    Time: 4 ms (81.74%), Space: 140 MB (47.39%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -8,8 +8,8 @@ public:
             if (nums[i] == 0) {
 
                 nums[i] = 1;
-                nums[i + 1] ^= 1;
-                nums[i + 2] ^= 1;
+                nums[i + 1] = nums[i+1] == 0?1:0;
+                nums[i + 2] = nums[i+2] == 0?1:0;
                 cnt++;
             }
         }
