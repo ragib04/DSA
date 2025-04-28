f5ba203ee53b5089e016e0db332ad7db0174dc6b
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-04-28 16:15:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -7,29 +7,27 @@ using namespace std;
 
 class Solution {
   public:
-  
-  int f(vector<int> &arr, int target,int ind, vector<vector<int>> &dp){
-      //if(target == 0) return 1;
-      if(ind == 0){
+  int cntsubset(int ind, vector<int> &arr, int target, vector<vector<int>> &dp)
+  {
+        if(ind == 0){
           if(target == 0 && arr[0] == 0) return 2;
           if(target == 0 || arr[0] == target) return 1;
           return 0;
       }
       
       if(dp[ind][target] != -1) return dp[ind][target];
-      int not_pick = f(arr, target,ind-1, dp);
-      int pick = 0;
-      if(arr[ind]<=target){
-          pick = f(arr, (target- arr[ind]),ind-1, dp);
-      }
       
-      return dp[ind][target] = pick + not_pick;
+      int not_pick = cntsubset(ind-1, arr, target, dp);
+      int pic = 0;
+      if(arr[ind]<=target) pic = cntsubset(ind-1,  arr, (target-arr[ind]), dp);
+      
+      return dp[ind][target] = pic + not_pick;
   }
     int perfectSum(vector<int>& arr, int target) {
-        int n = arr.size();
         // code here
+        int n = arr.size();
         vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return f(arr, target, n-1, dp);
+        return cntsubset(n-1, arr, target, dp);
     }
 };
 
