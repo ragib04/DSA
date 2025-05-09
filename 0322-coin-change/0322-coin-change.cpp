e967075b166da01be37fae5586faa70455a7655d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-05-09 09:53:05 +0530

    Time: 33 ms (53.36%), Space: 22.2 MB (45.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
 
@@ -20,6 +22,60 @@ int f(int ind, int t, vector<int> &coins, vector<vector<int>> &dp){
         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
         int ans = f(n-1, amount, coins, dp);
         if(ans >= 1e9) return -1;
+        return ans;
+    }
+};
+*/
+/*
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int target) {
+        int n = coins.size();
+        vector<vector<int> > dp(n, vector<int> (target+1, 0));
+        for(int t = 0; t<=target; t++){
+            if(t%coins[0] == 0) dp[0][t] = t/coins[0];
+            else dp[0][t] = 1e9;
+
+        }
+        for(int ind = 1; ind<n; ind++){
+            for(int t = 0; t<=target; target++){
+                int not_take = dp[ind-1][t];
+                int take = INT_MAX;
+                if(t>=coins[ind]) take = dp[ind][target-coins[ind]];
+
+                dp[ind][t] = min(take, not_take);
+
+            }
+        }
+        int ans = dp[n-1][target];
+        if(ans >= 1e9) return -1;
+        return ans;
+    }
+};
+*/
+
+class Solution {
+public:
+    int coinChange(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> prev(target+1, 0), curr(target+1, 0);
+        for(int t = 0; t<=target; t++){
+            if(t%nums[0] == 0) prev[t] = t/nums[0];
+            else prev[t] = 1e9;
+        }
+        for (int ind = 1; ind<n; ind++){
+            for(int t = 0; t<=target;t++){
+                int not_take = 0 + prev[t];
+                int take = 1e9;
+                if(nums[ind] <= t) take = 1 + curr[t-nums[ind]];
+
+                curr[t] = min(take, not_take);
+            }
+            prev = curr;
+        }
+        int ans = prev[target];
+        if(ans >= 1e9) return -1;
+
         return ans;
     }
 };
