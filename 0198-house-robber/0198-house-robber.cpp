8a14cc37395306fc2f26e248d1b2eff0d6229a0f
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-14 12:11:26 +0530

    Time: 0 ms (100%), Space: 10.7 MB (45.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,55 +1,20 @@
 class Solution {
 public:
-/*.  REcursive
-
-int hrob(int ind, vector<int> &nums){
+int robed(int ind, vector<int> &nums, vector<int> &dp){
     if(ind == 0) return nums[ind];
     if(ind<0) return 0;
 
-    int pick = nums[ind] + hrob(ind+2, nums);
-    int notpick = 0 + hrob(ind+1, nums);
+    if(dp[ind] != -1) return dp[ind];
 
-    return max(pick, notpick)
+    int pick = nums[ind] + robed(ind-2, nums, dp);
+    int not_pick = robed(ind-1, nums, dp);
+
+    dp[ind] = max(pick, not_pick);
+    return dp[ind];
 }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        return hrob(n-1, nums);
-    }*/
-/*
-    // memoization
-
-    int func(int ind, vector<int> &nums, vector<int> &dp){
-        if(ind<0) return 0;
-        if(ind == 0) return nums[ind];
-        if(dp[ind] != -1) return dp[ind];
-
-        int pick = nums[ind] + func(ind-2, nums, dp);
-        int notpick = 0+ func(ind-1, nums, dp);
-
-        return dp[ind] = max(pick, notpick);
-    }
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return func(n-1, nums, dp);
-    }*/
-
-
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        //vector<int> dp(n);
-        int prev1 = nums[0];
-        int prev2 = 0;
-       
-        for(int i = 1; i<n; i++){
-            int pick = nums[i];
-            if(i>1) pick += prev2;
-            int notpick = 0+prev1;
-
-            int curr = max(pick, notpick);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        int n= nums.size();
+        vector<int> dp(n+1, -1);
+        return robed(n-1, nums, dp);
     }
 };
