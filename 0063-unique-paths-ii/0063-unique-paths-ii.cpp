ceae06f1377829991e2bda9790255d7b7f20d4d2
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-15 15:44:12 +0530

    Time: 0 ms (100%), Space: 12 MB (28.04%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,40 @@
-/*
 class Solution {
 public:
-//memoization
-int solve(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
-   // memoization solution
+
+int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp){
+    if(i >= 0 && j >= 0 && grid[i][j] == 1) return 0;
+    
+    if(i == 0 && j== 0) return 1;
+    if(i < 0 || j < 0) return 0;
 
 
-        if(i >= 0 && j >= 0 && grid[i][j] == 1) return 0;
-       
-        if(i == 0 && j == 0) return 1;
-     
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j]; 
 
-        int up = solve(grid, i-1, j, dp);
-        int left = solve(grid, i, j-1, dp);
+    if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = up + left;
-    }
+
+
+   int up = solve(i-1, j, grid, dp);
+    int left = solve(i, j-1, grid, dp);
+
+    return dp[i][j] = up+left;
+
+    
+
+}
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n = grid.size(); // rows
         int m = grid[0].size(); // cols
         vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        return solve(grid, n-1, m-1, dp);
+        return solve(n-1, m-1, grid, dp);
     }
-
-};*/
-
+};
 
 
+
+
+
+/**
 // Tabulation->>>>>>>>>>>
 class Solution {
 public:
@@ -53,7 +58,7 @@ public:
         }
         return dp[n-1][m-1];
     }
-};
+};**/
 /*
 class Solution {
 public:
