33d2131ff4856d3fb32192682e348ed6f85b1a67
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-15 13:04:39 +0530

    Time: 1 ms (24.92%), Space: 9.4 MB (9.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,79 +1,17 @@
-/* Recursive---------->
-int f(int i, int j){
-    if(i == 0 && j==0) return 1;
-    if(i<0 || j<0) return 0;
-
-    int up = f(i-1, j);
-    int left = f(i, j-1);
-    return up+left;
-}
-    int uniquePaths(int m, int n) {
-        return f(m-1, n-1);
-    }
-*/
-/* memoization>>>>>>>>>
-
 class Solution {
 public:
-int f(int i, int j, vector<vector<int>> &dp){
-    if(i ==0 && j ==0) return 1;
-    if(i<0 || j<0) return 0;
-    if(dp[i][j] != -1) return dp[i][j];
 
-    int up = f(i-1, j, dp);
-    int left = f(i, j-1, dp);
+int solve(int i, int j, vector<vector<int>> &dp){
+    if(i == 0 && j == 0) return 1;
+    if(i<0 || j<0) return 0;
+if(dp[i][j] != -1) return dp[i][j];
+    int up = solve(i-1, j, dp);
+    int left = solve(i, j-1, dp);
 
     return dp[i][j] = up+left;
 }
     int uniquePaths(int m, int n) {
-    
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
-       
-    }
-};*/
-/*
-Tabulation -------->
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n,0));
-        dp[0][0] = 1;
-        for(int i =0; i<m; i++){
-            for(int j= 0; j<n; j++){
-                if(i == 0 && j == 0) dp[0][0] = 1;
-                else{
-                   int left = 0, up = 0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-
-                    dp[i][j] = left+up;
-                }
-            }
-        }
-        return dp[m-1][n-1];
-    }
-};*/
-
-//space optimization;
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<int> prev(n,0);
-        for(int i = 0; i<m; i++){
-            vector<int> curr(n,0);
-            for(int j = 0; j<n; j++){
-                if(i==0 && j==0) curr[j] =1;
-                else{
-                    int up =0 , left = 0;
-                    if(i>0) up = prev[j];
-                    if(j>0) left = curr[j-1];
-
-                    curr[j] = up+left;
-                }
-            }
-        prev = curr;
-        }
-        return prev[n-1];
+        vector<vector<int>> dp(m, vector<int>(n+1, -1));
+        return solve(m-1, n-1, dp);
     }
 };
