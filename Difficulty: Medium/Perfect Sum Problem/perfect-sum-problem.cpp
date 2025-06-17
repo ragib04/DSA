9e88f089e77eb50a2578ad2af5758004472b8c05
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-17 16:48:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -5,31 +5,33 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
-  int cntsubset(int ind, vector<int> &arr, int target, vector<vector<int>> &dp)
-  {
-        if(ind == 0){
-          if(target == 0 && arr[0] == 0) return 2;
-          if(target == 0 || arr[0] == target) return 1;
-          return 0;
-      }
-      
-      if(dp[ind][target] != -1) return dp[ind][target];
-      
-      int not_pick = cntsubset(ind-1, arr, target, dp);
-      int pic = 0;
-      if(arr[ind]<=target) pic = cntsubset(ind-1,  arr, (target-arr[ind]), dp);
-      
-      return dp[ind][target] = pic + not_pick;
-  }
     int perfectSum(vector<int>& arr, int target) {
         // code here
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return cntsubset(n-1, arr, target, dp);
+        vector<vector<int>>dp(n, vector<int>(target+1, 0));
+        
+        if(arr[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        
+        if(arr[0] != 0 && arr[0]<=target) dp[0][arr[0]] = 1;
+        
+        for(int ind = 1; ind<n; ind++){
+            for(int sum = 0; sum<=target; sum++){
+                int notTake = dp[ind-1][sum];
+                int Take = 0;
+                 if(arr[ind]<=sum) Take = dp[ind-1][sum-arr[ind]];
+                 
+                 dp[ind][sum] = Take + notTake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
+
+
 
 
 //{ Driver Code Starts.
