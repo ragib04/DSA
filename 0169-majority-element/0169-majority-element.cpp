f6e8373ed37809bdd4fd130e6b0cefac86375179
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-20 12:19:47 +0530

    Time: 0 ms (100%), Space: 28.2 MB (59.01%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        map<int,int> mpp;
+        for(auto i : nums){
+            mpp[i]++;
+        }
+        for(auto it: mpp){
+            if(it.second > (n/2)) return it.first;
+        }
+        return -1;
+    }
+};
