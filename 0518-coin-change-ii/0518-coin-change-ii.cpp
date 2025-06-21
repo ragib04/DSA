a9cc20e9a2f2e6300a5b9655fa4515ad65ff3e88
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-22 01:03:13 +0530

    Time: 29 ms (30.78%), Space: 46.2 MB (11.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -15,7 +15,23 @@ int func(int ind, int t,vector<int> &coins, vector<vector<long>> &dp){
 }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<long>> dp(n, vector<long>(amount+1, -1));
-        return func(n-1, amount, coins, dp);
+        vector<vector<long>> dp(n, vector<long>(amount+1, 0));
+        //return func(n-1, amount, coins, dp);
+        for(int i = 0; i<n; i++) dp[i][0] = 1;
+
+        for(int i = 0; i<amount+1; i++){
+            if(i%coins[0] == 0) dp[0][i] = 1;
+        }
+
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<=amount; j++){
+                long pick = 0;
+                long not_pick = dp[i-1][j];
+                if(coins[i]<=j) pick = dp[i][j-coins[i]];
+    dp[i][j] = (not_pick + pick > INT_MAX)? INT_MAX : not_pick+pick;
+
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
