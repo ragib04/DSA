a1c0b272154c32131e61e91e73ba5c5335b7d52f
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-23 13:33:15 +0530

    Time: 0 ms (100%), Space: 97.5 MB (26%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minsofar = prices[0];
+        int maxprofit = 0;
+        for(int i = 0; i<prices.size(); i++){
+            minsofar = min(minsofar,prices[i]);
+            maxprofit = max(maxprofit, prices[i]-minsofar);
+        }
+        return maxprofit;
+    }
+};
