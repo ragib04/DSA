9d3d6972073981cff652f58eba1d603ba56eebf9
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-06-25 21:36:11 +0530

    Time: 27 ms (55.81%), Space: 27.5 MB (28.02%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
+    int solve(int i, int j, string& x, string& y, vector<vector<int>>& dp) {
+        if (i == x.size() || j == y.size())
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int ans = 0;
+        if (x[i] == y[j]) {
+            ans = 1 + solve(i + 1, j + 1, x, y, dp);
+        } else {
+            ans = max(solve(i + 1, j, x, y, dp), solve(i, j + 1, x, y, dp));
+        }
 
-int solve(int i, int j, string &x, string& y, vector<vector<int>> &dp){
-    if(i == x.size() || j==y.size()) return 0;
-if(dp[i][j] != -1) return dp[i][j];
-int ans = 0;
-    if(x[i] == y[j]){
-        ans = 1 + solve(i+1, j+1, x, y, dp);
+        return dp[i][j] = ans;
     }
-    else{
-        ans = max(solve(i+1, j, x, y, dp), solve(i, j+1, x, y, dp));
-    }
-
-    return dp[i][j] = ans;
-}
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size(), vector<int> (text2.size(), -1));
-        return solve(0,0, text1, text2, dp);
+        // vector<vector<int>> dp(text1.size()+1, vector<int> (text2.size()+1,
+        // 0));
+        vector<vector<int>> dp(text1.size() + 1,
+                               vector<int>(text2.size() + 1, 0));
+        // return solve(0,0, text1, text2, dp);
 
+        for (int i = text1.length()-1; i >= 0; i--) {
+            for (int j = text2.length()-1; j >= 0; j--) {
+                int ans = 0;
+                if (text1[i] == text2[j]) {
+                    ans = 1 + dp[i + 1][j + 1];
+                } else {
+                    ans = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
     }
 };
