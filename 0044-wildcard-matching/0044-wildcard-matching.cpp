6bd80b39ba3c779ed9578f0cb3044a500bd7db17
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-07-01 13:04:32 +0530

    Time: 33 ms (51.87%), Space: 15 MB (57.37%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, string &p, vector<vector<int>> &dp) {
-        if(i == 0 && j == 0) return true;
-        if(j == 0 && i > 0) return false;
-        if(i == 0 && j > 0) {
-            for(int ind = 1; ind <= j; ind++) {
-                if(p[ind - 1] != '*') return false;
-            }
-            return true;
-        }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i - 1] == p[j - 1] || p[j - 1] == '?')
-            return dp[i][j] = solve(i - 1, j - 1, s, p, dp);
-
-        if(p[j - 1] == '*')
-            return dp[i][j] = solve(i - 1, j, s, p, dp) || solve(i, j - 1, s, p, dp);
-
-        return dp[i][j] = false;
-    }
-
     bool isMatch(string s, string p) {
         int n = s.length(), m = p.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(n, m, s, p, dp); // Pass full lengths now
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+        dp[0][0] = true;
+
+        // Only '*' can match empty string
+        for (int j = 1; j <= m; j++) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
