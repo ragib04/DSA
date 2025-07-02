992d82ce13054e044c8caf786d99d35c15589461
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-07-02 12:04:26 +0530

    Time: 0 ms (100%), Space: 17.1 MB (87.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -29,22 +29,26 @@ public:
        // vector<vector<int>> dp(n, vector<int>(2, -1));
         //return solve(0, 1, prices, n, dp);
 
-         vector<vector<long>> dp(n+1, vector<long>(2, 0));
-         dp[n][0] = dp[n][1] = 0;
+        // vector<vector<long>> dp(n+1, vector<long>(2, 0));
+        vector<long> head(2,0), curr(2,0);
+        head[0] = head[1] = 0;
+
+         //dp[n][0] = dp[n][1] = 0;
          for(int i = n-1; i>=0; i--){
             for(int buy = 0; buy<=1; buy++){
                   long profit = 0;
         if (buy) {
 
-            profit = max(-prices[i] + dp[i + 1][0],
-                         0 + dp[i + 1][1]);
+            profit = max(-prices[i] + head[0],
+                         0 + head[1]);
         } else {
-            profit = max(prices[i] + dp[i + 1][1],
-                         0 + dp[i + 1][0]);
+            profit = max(prices[i] + head[1],
+                         0 + head[0]);
         }
-        dp[i][buy] = profit;
+        curr[buy] = profit;
             }
+            head = curr;
          }
-         return dp[0][1];
+         return head[1];
     }
 };
