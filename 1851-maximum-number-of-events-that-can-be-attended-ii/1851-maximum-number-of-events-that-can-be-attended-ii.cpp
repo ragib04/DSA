b33cf702e2d19cf60d89133b29b646df57621cb2
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-07-08 13:26:17 +0530

    Time: 115 ms (75.66%), Space: 126.8 MB (19.93%) - LeetHub

---
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+
+public:
+
+int n;
+int func(int ind, vector<vector<int>> &event, int k, vector<vector<int>> &dp){
+    if(k == 0 || ind>=n) return 0;
+
+    int start = event[ind][0];
+    int end = event[ind][1];
+    int value = event[ind][2];
+if(dp[ind][k] != -1) return dp[ind][k];
+
+    int not_take = func(ind+1, event, k, dp);
+    int j = ind+1;
+    for( ; j<n; j++){
+        if(event[j][0]>event[ind][1]) break;
+    }
+    int take = value + func(j, event, k-1, dp);
+
+    return dp[ind][k] = max(take, not_take);
+}
+    int maxValue(vector<vector<int>>& events, int k) {
+      n = events.size();
+        //if(k>n) return 0;
+        vector<vector<int>> dp(n, vector<int>(k+1, -1));
+        sort(events.begin(), events.end());
+        return func(0,events, k, dp);
+
+    }
+};
