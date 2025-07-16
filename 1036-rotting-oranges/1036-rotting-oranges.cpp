ffd45492be75efc1fc251d0e3ae192c4f75cba4d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-07-16 22:44:31 +0530

    Time: 0 ms (100%), Space: 16.7 MB (88.32%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -4,8 +4,10 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<pair<int, int>, int>> q;
-        //vector<vector<int>> vis;
+       // vector<vector<int>> vis;
         int vis[n][m];
+        int cntfresh = 0;
+
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(grid[i][j] == 2){
@@ -16,11 +18,13 @@ public:
                 else{
                     vis[i][j] = 0;
                 }
+                if(grid[i][j] == 1) cntfresh++;
             }
         }
         int drow[] = {-1,0,+1,0};
         int dcol[] = {0,1,0,-1};
         int time= 0;
+        int cnt = 0;
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
@@ -36,15 +40,17 @@ public:
                     q.push({{nrow, ncol}, t+1});
 
                     vis[nrow][ncol] = 2;
+                    cnt++;
                 }
             }
         }
 // to check ans fresh orange is remaining?
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(vis[i][j] != 2 && grid[i][j] == 1) return -1;
-            }
-        }
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         if(vis[i][j] ! = 2 && grid[i][j] == 1) return -1;
+        //     }
+        // }
+        if(cnt != cntfresh) return -1;
         return time;
     }
 };
