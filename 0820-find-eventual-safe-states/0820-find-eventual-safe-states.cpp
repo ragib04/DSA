a5182fc4c39e37ac738a1e9bbd6878d89d94aadc
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-02 00:15:23 +0530

    Time: 63 ms (12.74%), Space: 63.6 MB (58.85%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-bool dfscheckcycle(int node, vector<bool> &vis, vector<bool>&pathvis, vector<int> &check, vector<vector<int>> &graph){
-    vis[node] = 1;
-    pathvis[node] = 1;
-    for(auto neighbour: graph[node]){
-        if(!vis[neighbour]){
-            if(dfscheckcycle(neighbour, vis, pathvis, check, graph) == true) return true;
-        }
-        else if(pathvis[neighbour]) return true;
-    }
-    check[node] = 1;
-    pathvis[node] = 0;
-    return false;
-}
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> vis(n, 0);
-        vector<bool> pathvis(n, 0);vector<int> check(n, 0);
-        vector<int> safenode;
-        for(int i = 0; i<n ; i++){
-            if(!vis[i]){
-                dfscheckcycle(i, vis, pathvis, check, graph);
+        vector<int> revadj[n];
+        vector<int> indegree(n, 0);
+        for(int i = 0; i<n; i++){
+            // i->it => it -> i;
+            for(auto it: graph[i]){
+
+                revadj[it].push_back(i);
+                indegree[i]++;
             }
         }
+        queue<int> q;
         for(int i = 0; i<n; i++){
-            if(check[i] == 1) safenode.push_back(i);
+            if(indegree[i] == 0) q.push(i);
         }
-        return safenode;
+        vector<int> safenodes;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safenodes.push_back(node);
+            for(auto it: revadj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+
+            }
+        }
+        sort(safenodes.begin(), safenodes.end());
+        return safenodes;
     }
 };
