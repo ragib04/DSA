327bed3f8a8b4f00de1c371e59623ba9c0c0c118
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-09 10:43:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.81%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0) n = n/2;
-        return n==1;
+        if(n<=0) return false;
+        //applying AND operation firstly   /covert in Binary form the apply and if is 0 return true
+        return (n&(n-1)) == 0;    
     }
 };
