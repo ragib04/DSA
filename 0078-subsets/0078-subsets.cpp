f4f10b5180ed082e146e05329589fbb1ade215f0
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-13 11:58:46 +0530

    Time: 3 ms (31.93%), Space: 15.7 MB (16.04%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,22 @@
 class Solution {
+
 public:
 
-void solve(vector<int> &nums, vector<int> output, int ind, vector<vector<int>> &ans){
-    if(ind>=nums.size()){
-        ans.push_back(output);
+void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> path,int i){
+    if(i>=nums.size()){
+        ans.push_back(path);
         return;
     }
- solve(nums,output,ind+1, ans);
-    
-    output.push_back(nums[ind]);
-    solve(nums, output, ind+1, ans);
-
-  
+    //skip
+    solve(nums, ans, path, i+1);
+    path.push_back(nums[i]);
+    solve(nums, ans, path, i+1);
 }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> output;
-        int ind = 0;
-        solve(nums, output,ind, ans);
-    return ans;
+        vector<int> path;
+        solve(nums, ans, path, 0);
+        return ans;
     }
 };
