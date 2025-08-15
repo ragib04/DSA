17bf31cffebdf55627b598e3468c8a5f27f5be4d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-15 11:00:19 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.36%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0) return false;
-       return (n & n-1) == 0 && (n-1)%3 == 0;
+       while(n%4 == 0){
+        n /= 4;
+       }
+       if(n == 1) return true;
+       return false;
     }
 };
