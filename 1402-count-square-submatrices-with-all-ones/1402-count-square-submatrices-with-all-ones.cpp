e708f4f53b27e563cd4ecd9eed9b8bd3735ad31f
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-22 10:09:50 +0530

    Time: 1 ms (90.33%), Space: 30.4 MB (39.63%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-int solve(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp){
+// int solve(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp){
     
-    int n = matrix.size(), m = matrix[0].size();
-    if(i>=n || j>= m) return 0;
-    if(dp[i][j] != -1) return dp[i][j];
-    int left = solve(matrix, i, j+1, dp);
-    int diagonal = solve(matrix, i+1, j+1, dp);
-    int down = solve(matrix, i+1, j, dp);
+//     int n = matrix.size(), m = matrix[0].size();
+//     if(i>=n || j>= m) return 0;
+//     if(dp[i][j] != -1) return dp[i][j];
+//     int left = solve(matrix, i, j+1, dp);
+//     int diagonal = solve(matrix, i+1, j+1, dp);
+//     int down = solve(matrix, i+1, j, dp);
 
-    if(matrix[i][j] == 1){
-        dp[i][j] = 1+min(left, min(diagonal, down));
-        return dp[i][j];
-    }
-    else return dp[i][j] = 0;
+//     if(matrix[i][j] == 1){
+//         dp[i][j] = 1+min(left, min(diagonal, down));
+//         return dp[i][j];
+//     }
+//     else return dp[i][j] = 0;
 
     
-}
+// }
     int countSquares(vector<vector<int>>& matrix) {
-        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
-        solve(matrix, 0,0, dp);
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 0));
+        //solve(matrix, 0,0, dp);
+        //Tabu
+        //basee case
+        for(int i = 0; i<matrix.size(); i++) dp[i][0] = matrix[i][0];
+        for(int j = 0; j<matrix[0].size(); j++) dp[0][j] = matrix[0][j];
+
+        for(int i = 1; i<matrix.size(); i++){
+            for(int j = 1; j<matrix[0].size(); j++){
+                
+                if(matrix[i][j] == 1) dp[i][j] = 1+ min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            }
+        }
         int sum = 0;
         for(int i = 0; i<matrix.size(); i++){
             for(int j = 0; j<matrix[0].size(); j++){
