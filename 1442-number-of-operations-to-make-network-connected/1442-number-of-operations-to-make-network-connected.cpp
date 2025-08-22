2d4b005170f7c177c7aeb49b6ee0c3f5ca2e8184
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-22 22:52:14 +0530

    Time: 1 ms (91.27%), Space: 42.1 MB (92.28%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    class Disjointset {
+    public:
+        vector<int> parent, rank;
+
+        Disjointset(int n) {
+            parent.resize(n);
+            rank.resize(n, 0);
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int findparent(int node) {
+            if (parent[node] == node) return node;
+            return parent[node] = findparent(parent[node]);
+        }
+
+        void unionset(int u, int v) {
+            u = findparent(u);
+            v = findparent(v);
+
+            if (u == v) return;
+
+            if (rank[u] < rank[v]) {
+                parent[u] = v;
+            }
+            else if (rank[u] > rank[v]) {
+                parent[v] = u;
+            }
+            else {
+                parent[v] = u;
+                rank[u]++;
+            }
+        }
+    };
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) return -1; // not enough edges
+
+        Disjointset ds(n);
+        int cntextraedges = 0;
+
+        for (auto &it : connections) {
+            int u = it[0];
+            int v = it[1];
+
+            if (ds.findparent(u) == ds.findparent(v)) {
+                cntextraedges++;
+            } else {
+                ds.unionset(u, v);
+            }
+        }
+
+        int components = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent[i] == i) components++;
+        }
+
+        int required = components - 1;
+        if (cntextraedges >= required) return required;
+        else return -1;
+    }
+};
