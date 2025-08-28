6079359d11eae3c897683c29608c89544f4e96b1
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-28 12:45:16 +0530

    Time: 23 ms (5.76%), Space: 46.6 MB (5.56%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        map<int, vector<int>> mpp;
-        int r = grid.size(), c = grid[0].size();
-
+        int r = grid.size();
+        int c = grid[0].size();
+        map<int, vector<int>>mp;
         for(int i = 0; i<r; i++){
             for(int j = 0; j<c; j++){
-                mpp[i-j].push_back(grid[i][j]);
+                mp[i-j].push_back(grid[i][j]);
             }
         }
-        for(auto &entry : mpp){
-            if(entry.first<0) sort(entry.second.begin(), entry.second.end());
-            else sort(entry.second.rbegin(),entry.second.rend());
+        for(auto &it: mp){
+            if(it.first<0){
+                sort(it.second.begin(), it.second.end());
+            }
+            else{
+                sort(it.second.rbegin(), it.second.rend());
+            }
         }
-
         for(int i = 0; i<r; i++){
             for(int j = 0; j<c; j++){
-                grid[i][j] = mpp[i-j].front();
-                mpp[i-j].erase(mpp[i-j].begin());
+                grid[i][j] = mp[i-j].front();
+                mp[i-j].erase(mp[i-j].begin());
             }
         }
         return grid;
