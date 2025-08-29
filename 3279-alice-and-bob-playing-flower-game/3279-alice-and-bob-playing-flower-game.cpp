c0640a366a353aa073caf7674efaaed3af7f5342
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-08-29 11:50:07 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.89%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
 
     long long flowerGame(int n, int m) {
-        return ((1LL*n*m)/2);
+       long oddx = (n+1)/2;
+       long evenx = n/2;
+
+       long oddy = (m+1)/2;
+       long eveny = m/2;
+
+       return (oddx*eveny)+(evenx*oddy);
     }
 };
