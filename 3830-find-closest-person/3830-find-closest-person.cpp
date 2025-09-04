6d007a05d406866c66d2a39e550a9e7784e161de
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-04 12:24:13 +0530

    Time: 0 ms (100%), Space: 8.8 MB (0.68%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        if((x==z) && (y == z)) return 0;
-        if((x == z)) return 1;
-        if(y == z) return 2;
-        int p1 = abs(x-z);
-        int p2 = abs(y-z);
-        if(p1<p2) return 1;
-        if(p1>p2) return 2;
-
-        return 0;
-
+        // if((x==z) && (y == z)) return 0;
+        // if((x == z)) return 1;
+        // if(y == z) return 2;
+        // int p1 = abs(x-z);
+        // int p2 = abs(y-z);
+        // if(p1<p2) return 1;
+        // if(p1>p2) return 2;
+        // return 0;
+       
+        return abs(x-z) == abs(y-z)?0: abs(x-z)>abs(y-z)?2:1;
 
     }
 };                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
