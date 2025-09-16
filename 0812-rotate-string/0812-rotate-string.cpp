11da136954219f047230efe4f721d227a71694f8
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-16 23:40:10 +0530

    Time: 0 ms (100%), Space: 8 MB (84.06%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool rotateString(string s, string goal) {
@@ -8,5 +9,20 @@ public:
         string concat = s+s;
         return concat.find(goal) != string::npos;
        
+    }
+};
+*/
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+        int m = goal.size();
+        if(n != m) return false;
+        for(int i = 0; i<n; i++){
+            rotate(s.begin(),s.begin()+1 ,s.end());
+            if(s == goal) return true;
+        }
+        return false;
     }
 };
