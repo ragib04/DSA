07b5c6710a95a4d521055b4348334b8b19ffe08e
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-17 16:12:07 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -6,5 +7,27 @@ public:
     sort(t.begin(), t.end());
 
         return (s==t);
+    }
+};
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        if(n != m) return false;
+        vector<int> freq(26, 0);
+        for(int i = 0; i<n; i++){
+            freq[s[i] - 'a']++;
+
+        }
+         for(int i = 0; i<n; i++){
+            freq[t[i] - 'a']--;
+
+        }
+        for(int i = 0; i<26; i++){
+            if(freq[i] != 0) return false;
+        }
+        return true;
     }
 };
