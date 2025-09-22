da0c6b9afc992ae251072b8bba83ff1405306fba
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-22 12:24:42 +0530

    Time: 0 ms (100%), Space: 23.2 MB (67.03%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
+        vector<int> freq(101, 0);
+        for(int i = 0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        int maxi = *max_element(freq.begin(), freq.end());
+        int res = 0;
+        for(int i = 0; i<101; i++){
+            if(freq[i] == maxi) res += freq[i];
+        }
+        return res;
+    }
+};
+
+
+
+/*
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
         unordered_map<int, int> mp;
         for(auto it: nums){
             mp[it]++;
@@ -21,3 +40,4 @@ public:
         return cnt;
     }
 };
+*/
