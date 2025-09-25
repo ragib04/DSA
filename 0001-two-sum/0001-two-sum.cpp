85ab894098bf9540470546acb53fdc6b06c07948
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-25 11:50:34 +0530

    Time: 8 ms (43.67%), Space: 15.8 MB (8.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,26 +3,26 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         //brute force
+        // for(int i = 0; i<nums.size(); i++){
+        //     for(int j = i+1; j<nums.size();j++){
+        //         if(nums[j] == target-nums[i]){
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // return {};
+        unordered_map<int, int> mp;
         for(int i = 0; i<nums.size(); i++){
-            for(int j = i+1; j<nums.size();j++){
-                if(nums[j] == target-nums[i]){
-                    return {i, j};
-                }
+            mp[nums[i]] = i;
+
+        }
+        for(int i = 0; i<nums.size(); i++){
+            int remaning = target- nums[i];
+            if(mp.find(remaning) != mp.end() && i != mp[remaning]){
+                
+                return {i, mp[remaning]};
             }
         }
         return {};
-        // unordered_map<int, int> numMap;
-        // int n = nums.size();
-
-        // for (int i = 0; i < n; i++) {
-        //     int complement = target - nums[i];
-        //     if (numMap.count(complement)) {
-        //         return {numMap[complement], i};
-        //     }
-        //     numMap[nums[i]] = i;
-        // }
-
-        // return {}; // No solution found
-   // }
     }
 };
