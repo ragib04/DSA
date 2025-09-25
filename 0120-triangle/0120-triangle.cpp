cae4d07d543f549225bbeedd8831df654669baae
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-25 10:20:12 +0530

    Time: 0 ms (100%), Space: 12.7 MB (72.83%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,6 +1,3 @@
-/*
-
-//memoization
 class Solution {
 public:
 
@@ -16,59 +13,37 @@ int f(int i, int j, vector<vector<int>> &triangle, int n, vector<vector<int>> &d
 }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, 0, triangle, n, dp);
-    }
-};
-
-
-//Tabulation
-class Solution {
-public:
-
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for(int j = 0; j<n; j++){
-            dp[n-1][j] = triangle[n-1][j];
-            }
-            for(int i = n-2; i>=0; i--){
-                for(int j = i; j>= 0; j--){
-                
-                int down = triangle[i][j] +  dp[i+1][j];
-                int diag = triangle[i][j] +  dp[i+1][j+1];
-
-                dp[i][j] = min(down, diag);
-                }
-            }
+        // vector<vector<int>> dp(n, vector<int>(n, 0));
+        // for(int j = 0; j<n; j++){
+        //     dp[n-1][j] = triangle[n-1][j];
+        // }
+        //     for(int i = n-2; i>=0; i--){
+        //         for(int j = i; j>= 0; j--){
+        //         int down = triangle[i][j] +  dp[i+1][j];        
+        //         int diag = triangle[i][j] +  dp[i+1][j+1];
+        //         dp[i][j] = min(down, diag);
+        //         /*
+        //         [11]
+        //         [9 10]
+        //         [7, 6 10]
+        //         [4 1 8 3]*/
+        //         }
+        //     }
         
-        return dp[0][0];
-    }
-};
-*/
-
-
-class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<int> front(n,0), curr(n,0);
-
+        // return dp[0][0];
+        vector<int>curr(n,0), prev(n,0);
         for(int j = 0; j<n; j++){
-            front[j] = triangle[n-1][j];
+            prev[j] = triangle[n-1][j];
         }
-            for(int i = n-2; i>=0; i--){
-                
-                for(int j = i; j>= 0; j--){
-                
-                int down = triangle[i][j] +  front[j];
-                int diag = triangle[i][j] +  front[j+1];
+        for(int i = n-2; i>=0; i--){
+            for(int j = i; j>=0; j--){
+                int down = triangle[i][j] + prev[j];
+                int diag = triangle[i][j] + prev[j+1];
 
                 curr[j] = min(down, diag);
-                }
-                front = curr;
             }
-        
-        return front[0];
+            prev = curr;
+        }
+        return prev[0];
     }
 };
