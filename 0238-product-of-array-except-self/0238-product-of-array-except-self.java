7602f295736020c6b183117039904abc9b6f2a88
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-25 14:15:17 +0530

    Time: 2 ms (87.1%), Space: 55.6 MB (50.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,75 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1; //[1 1 2 6]
-        suff[n - 1] = 1;    //[24 12 4 1]
+        // int pre[] = new int[n];
+        // int suff[] = new int[n];
+        // pre[0] = 1; //[1 1 2 6]
+        // suff[n - 1] = 1;    //[24 12 4 1]
         
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
-        }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
-        }
+        // for(int i = 1; i < n; i++) {
+        //     pre[i] = pre[i - 1] * nums[i - 1];
+        // }
+        // for(int i = n - 2; i >= 0; i--) {
+        //     suff[i] = suff[i + 1] * nums[i + 1];
+        // }
         
-        int ans[] = new int[n];     // [24 12 8 6]
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
+        // int ans[] = new int[n];     // [24 12 8 6]
+        // for(int i = 0; i < n; i++) {
+        //     ans[i] = pre[i] * suff[i];
+        // }
+        // return ans;
+     
+        int ans[] = new int[n];
+        ans[0] = 1;
+        for(int i = 1; i<n; i++){
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+        int suffix = 1;
+        for(int i = n-2; i>=0; i--){
+            suffix *= nums[i+1];
+            ans[i] *= suffix;
         }
         return ans;
     }
 }
+
+// *******optimal t.c -> n
+/*
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        int pro = 1;
+        for(int i : nums) {
+            pro *= i;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            ans[i] = pro / nums[i];
+        }
+        return ans;
+    }
+}
+ */
+
+
+ // Brute force. t.c -> n^2
+ /**
+ class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            int pro = 1;
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                pro *= nums[j];
+            }
+            ans[i] = pro;
+        }
+        
+        return ans;
+    }
+}
+  */
