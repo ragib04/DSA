cf16d2d387f40f8a6ee4eae6b9df61d24630a48a
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-09-25 10:46:39 +0530

    Time: 87 ms (10.93%), Space: 14.1 MB (78.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,42 +1,28 @@
-/*
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
 
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap[nums[i]] = i;
-        }
-
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement) && numMap[complement] != i) {
-                return {i, numMap[complement]};
+        //brute force
+        for(int i = 0; i<nums.size(); i++){
+            for(int j = i+1; j<nums.size();j++){
+                if(nums[j] == target-nums[i]){
+                    return {i, j};
+                }
             }
         }
+        return {};
+        // unordered_map<int, int> numMap;
+        // int n = nums.size();
 
-        return {}; // No solution found
-    }
-};
-*/
+        // for (int i = 0; i < n; i++) {
+        //     int complement = target - nums[i];
+        //     if (numMap.count(complement)) {
+        //         return {numMap[complement], i};
+        //     }
+        //     numMap[nums[i]] = i;
+        // }
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement)) {
-                return {numMap[complement], i};
-            }
-            numMap[nums[i]] = i;
-        }
-
-        return {}; // No solution found
+        // return {}; // No solution found
+   // }
     }
 };
