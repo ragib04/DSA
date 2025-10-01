a312608160c1c1e3becf633a393b83450fee1e55
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-01 14:38:52 +0530

    Time: 0 ms (100%), Space: 8.3 MB (48.42%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -19,11 +19,16 @@ public:
 
 
         //App------>3  MATHS APP;
-        int cnt = 0;
-        while(n != 0){
-            cnt += n%2;
-            n /= 2;
-        }
-        return cnt;
+        // int cnt = 0;
+        // while(n != 0){
+        //     cnt += n%2;
+        //     n /= 2;
+        // }
+        // return cnt;
+
+
+        // App-------> 4  Built in Function
+        return __builtin_popcount(n);
+
     }
 };
