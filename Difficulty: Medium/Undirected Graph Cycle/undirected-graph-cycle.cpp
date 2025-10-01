1e6305e3da0a3e7a88c57969e1689680b80cd981
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-01 17:02:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,44 +1,38 @@
 class Solution {
   public:
-   
-    bool check(int i,vector<int>& vis,vector<vector<int>>& adj){
-        vis[i] = 1;
-        queue<pair<int,int>>q;
-        q.push({i,-1});
-        
-        while(!q.empty()){
-            int node = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            for(auto it : adj[node]){
-                if(!vis[it]){
-                    vis[it] = 1;
-                    q.push({it,node});
-                }
-                else if(parent!=it)
-                    return true;
-            }
-        }
-        return false;
-    }
+  
+  bool detectCycle(int src, int vis[], vector<vector<int>> &adj){
+      vis[src] = 1;
+      queue<pair<int, int>> q;
+      q.push({src, -1});
+      while(!q.empty()){
+          int node = q.front().first;
+          int parent = q.front().second;
+          q.pop();
+          for(auto adjnode: adj[node]){
+              if(!vis[adjnode]){
+                  vis[adjnode] = 1;
+                  q.push({adjnode, node});
+              }
+              else{
+                  if(parent != adjnode) return true;
+              }
+          }
+      }
+      return false;
+  }
     bool isCycle(int V, vector<vector<int>>& edges) {
-
+        // Code here
         vector<vector<int>> adj(V);
-        for (auto& edge : edges) {
+        for(auto &edge: edges){
             int u = edge[0], v = edge[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        
-        vector<int>vis(V,0);
-        
-        for(int i=0;i<V;i++){
-            
+        int vis[V] = {0};
+        for(int i = 0; i<V; i++){
             if(!vis[i]){
-                
-                if(check(i,vis,adj)){
-                    return true;
-                }
+                if(detectCycle(i, vis, adj)) return true;
             }
         }
         return false;
