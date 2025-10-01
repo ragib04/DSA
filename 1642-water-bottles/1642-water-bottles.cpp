e3dd1e03d3c6cce249f9d2b7b52ec9f2242aa54b
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-01 11:31:25 +0530

    Time: 0 ms (100%), Space: 7.8 MB (45.09%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -3,6 +3,13 @@ public:
     int numWaterBottles(int numBottles, int numExchange) {
         // return (numBottles*numExchange-1)/(numExchange-1);
         int n = numBottles, x = numExchange;
-        return n + (n-1)/(x-1);
+        int cnt = n;int r = 0;
+        while(n>=x){
+            r = n%x;
+            n = n/x;
+            cnt += n;
+            n += r;
+        }
+        return cnt;
     }
 };
