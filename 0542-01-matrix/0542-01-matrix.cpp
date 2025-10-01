057fe434a4bb1d3b57f8aa52008fa7517d9e7cfb
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-01 17:58:50 +0530

    Time: 30 ms (22.89%), Space: 38.7 MB (43.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    vector<int> DIR = {0, 1, 0, -1, 0};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> q;
-        for (int r = 0; r < m; ++r)
-            for (int c = 0; c < n; ++c)
-                if (mat[r][c] == 0) q.emplace(r, c);
-                else mat[r][c] = -1; // Marked as not processed yet!
-
-        while (!q.empty()) {
-            auto [r, c] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
-                mat[nr][nc] = mat[r][c] + 1;
-                q.emplace(nr, nc);
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        vector<vector<int>> dist(n, vector<int>(m,0));
+        queue<pair<pair<int, int>, int>> q;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(mat[i][j] == 0){
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+                else vis[i][j] = 0;
             }
         }
-        return mat;
+
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, 0, -1};
+        while(!q.empty()){
+            int row  = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            q.pop();
+            dist[row][col] = steps;
+            for(int i = 0; i<4; i++){
+                int nrow = row+delrow[i];
+                int ncol = col+delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps+1});
+                }
+            }
+        }
+        return dist;
     }
 };
