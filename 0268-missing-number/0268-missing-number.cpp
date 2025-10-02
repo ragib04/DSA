c9d9c5f6a303c8cf4e2757793ca6d0385a907c61
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-02 14:45:07 +0530

    Time: 14 ms (11.05%), Space: 28.2 MB (5.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        unordered_map<int, int> mp;
+        for(auto it: nums){
+            mp[it]++;
+        }
         int ans;
-        for(int i = 0; i<nums.size(); i++){
-            
-            if(nums[i] != i){
+        for(int i = 0; i<=nums.size(); i++){
+            if(mp.find(i) == mp.end()){
                 ans = i;
                 break;
             }
-           
         }
         return ans;
     }
