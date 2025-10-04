0ca4e8656a806ccc604529c78dc5ea02e37a0bc4
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-04 16:49:20 +0530

    Time: 42 ms (45.38%), Space: 49.4 MB (30.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -54,28 +54,56 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     int coinChange(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         vector<int> prev(target+1, 0), curr(target+1, 0);
+//         for(int t = 0; t<=target; t++){
+//             if(t%nums[0] == 0) prev[t] = t/nums[0];
+//             else prev[t] = 1e9;
+//         }
+//         for (int ind = 1; ind<n; ind++){
+//             for(int t = 0; t<=target;t++){
+//                 int not_take = 0 + prev[t];
+//                 int take = 1e9;
+//                 if(nums[ind] <= t) take = 1 + curr[t-nums[ind]];
+
+//                 curr[t] = min(take, not_take);
+//             }
+//             prev = curr;
+//         }
+//         int ans = prev[target];
+//         if(ans >= 1e9) return -1;
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    int coinChange(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> prev(target+1, 0), curr(target+1, 0);
-        for(int t = 0; t<=target; t++){
-            if(t%nums[0] == 0) prev[t] = t/nums[0];
-            else prev[t] = 1e9;
-        }
-        for (int ind = 1; ind<n; ind++){
-            for(int t = 0; t<=target;t++){
-                int not_take = 0 + prev[t];
-                int take = 1e9;
-                if(nums[ind] <= t) take = 1 + curr[t-nums[ind]];
+int solve(int i, int amnt, vector<int> &coins, vector<vector<int>> &dp){
+    if(i == 0){
+        if(amnt%coins[i] == 0) return amnt/coins[i];
+        else return 1e9;
 
-                curr[t] = min(take, not_take);
-            }
-            prev = curr;
-        }
-        int ans = prev[target];
-        if(ans >= 1e9) return -1;
+    }
+    if(dp[i][amnt] != -1) return dp[i][amnt];
+    int not_take = solve(i-1, amnt, coins, dp);
+    int take  = INT_MAX;
+    if(amnt>=coins[i]){
+        take = 1 + solve(i, amnt-coins[i], coins, dp);
+    }
 
+    return dp[i][amnt] = min(take, not_take);
+    
+}
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans  = solve(n-1, amount, coins, dp);
+        if(ans>=1e9) return -1;
         return ans;
     }
 };
