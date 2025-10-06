c314e02630fc5479f00a87c6368efae27ee9ea16
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-06 14:56:17 +0530

    Time: 293 ms (32.18%), Space: 296.5 MB (23.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,9 +13,26 @@ int solve(int i, int prev, vector<int> &nums, int n, vector<vector<int>> &dp){
     return dp[i][prev+1] = max(take, not_take);
 
 }
+
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solve(0, -1, nums, n, dp);
+
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+         // return solve(0, -1, nums, n, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prev = i - 1; prev >= -1; prev--) {
+                int not_take = dp[i + 1][prev + 1];
+                int take = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    take = 1 + dp[i + 1][i + 1];
+                }
+                dp[i][prev + 1] = max(take, not_take);
+            }
+        }
+
+        return dp[0][0];
     }
 };
