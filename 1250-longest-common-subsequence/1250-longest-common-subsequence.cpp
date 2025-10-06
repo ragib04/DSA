f7967f1bb5d883cee642cf18a78ab5ab3f380a39
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-06 15:58:18 +0530

    Time: 43 ms (24.27%), Space: 27.6 MB (29.51%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    int solve(int i, int j, string& x, string& y, vector<vector<int>>& dp) {
-        if (i == x.size() || j == y.size())
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int ans = 0;
-        if (x[i] == y[j]) {
-            ans = 1 + solve(i + 1, j + 1, x, y, dp);
-        } else {
-            ans = max(solve(i + 1, j, x, y, dp), solve(i, j + 1, x, y, dp));
-        }
-
-        return dp[i][j] = ans;
+    int solve(int i, int j, string &x, string& y, vector<vector<int>> &dp){
+    if(i == x.size() || j==y.size()) return 0;
+if(dp[i][j] != -1) return dp[i][j];
+int ans = 0;
+    if(x[i] == y[j]){
+        ans = 1 + solve(i+1, j+1, x, y, dp);
     }
+    else{
+        ans = max(solve(i+1, j, x, y, dp), solve(i, j+1, x, y, dp));
+    }
+
+    return dp[i][j] = ans;
+}
     int longestCommonSubsequence(string text1, string text2) {
-        // vector<vector<int>> dp(text1.size()+1, vector<int> (text2.size()+1,
-        // 0));
+        vector<vector<int>> dp(text1.size()+1, vector<int> (text2.size()+1,
+        -1));
         //vector<vector<int>> dp(text1.size() + 1,vector<int>(text2.size() + 1, 0));
-        // return solve(0,0, text1, text2, dp);
-vector<int> curr(text2.length() + 1, 0), next(text2.length() + 1, 0);
-        for (int i = text1.length()-1; i >= 0; i--) {
-            for (int j = text2.length()-1; j >= 0; j--) {
-                int ans = 0;
-                if (text1[i] == text2[j]) {
-                    ans = 1 + next[j + 1];
-                } else {
-                    ans = max(next[j], curr[j + 1]);
-                }
-curr[j] = ans;
-            }
-            next = curr;
-        }
-        return next[0];
+        return solve(0,0, text1, text2, dp);
+// vector<int> curr(text2.length() + 1, 0), next(text2.length() + 1, 0);
+//         for (int i = text1.length()-1; i >= 0; i--) {
+//             for (int j = text2.length()-1; j >= 0; j--) {
+//                 int ans = 0;
+//                 if (text1[i] == text2[j]) {
+//                     ans = 1 + next[j + 1];
+//                 } else {
+//                     ans = max(next[j], curr[j + 1]);
+//                 }
+// curr[j] = ans;
+//             }
+//             next = curr;
+//         }
+//         return next[0];
     }
 };
