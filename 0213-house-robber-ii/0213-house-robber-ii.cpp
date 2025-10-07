9ac0f406f07c015406e0a0468774238012b5f960
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-07 15:01:15 +0530

    Time: 3 ms (7.47%), Space: 10.9 MB (20.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
 
+int robed(int i, vector<int> &nums, vector<int>&dp){
+    if(i == 0) return nums[i];
+    if(i<0) return 0;
+    if(dp[i] != -1) return dp[i];
+    int pick = nums[i]+ robed(i-2, nums, dp);
+    int not_pick = robed(i-1, nums, dp);
+
+    return dp[i] = max(pick, not_pick);
+}
 int robber(vector<int> &nums){
     int n = nums.size();
 
-    vector<int> dp(n+1, 0);
-    dp[0] = nums[0];
-    for(int i = 1; i<n; i++){
-        int pick = nums[i];
-        if(i>1) pick += dp[i-2];
-        int not_pick = dp[i-1];
+    vector<int> dp(n+1, -1);
+    return robed(n-1, nums, dp);
 
-        dp[i] = max(pick, not_pick);
-    }
-    return dp[n-1];
+    // vector<int> dp(n+1, 0);
+    // dp[0] = nums[0];
+    // for(int i = 1; i<n; i++){
+    //     int pick = nums[i];
+    //     if(i>1) pick += dp[i-2];
+    //     int not_pick = dp[i-1];
+
+    //     dp[i] = max(pick, not_pick);
+    // }
+    // return dp[n-1];
     
 //     int prev1 = nums[0], prev2 = 0;
 //     for(int i = 1; i<n; i++){
