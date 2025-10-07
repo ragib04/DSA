8978b8fe940a42cebd2b8008c512e8af7748b58c
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-07 14:59:50 +0530

    Time: 0 ms (100%), Space: 10.9 MB (20.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -3,17 +3,28 @@ public:
 
 int robber(vector<int> &nums){
     int n = nums.size();
-    
-    int prev1 = nums[0], prev2 = 0;
+
+    vector<int> dp(n+1, 0);
+    dp[0] = nums[0];
     for(int i = 1; i<n; i++){
-        int take = nums[i];
-        if(i>1) take += prev2;
-        int nottake = prev1;
-        int curr = max(take, nottake);
-        prev2 = prev1;
-        prev1 = curr;
+        int pick = nums[i];
+        if(i>1) pick += dp[i-2];
+        int not_pick = dp[i-1];
+
+        dp[i] = max(pick, not_pick);
     }
-return prev1;
+    return dp[n-1];
+    
+//     int prev1 = nums[0], prev2 = 0;
+//     for(int i = 1; i<n; i++){
+//         int take = nums[i];
+//         if(i>1) take += prev2;
+//         int nottake = prev1;
+//         int curr = max(take, nottake);
+//         prev2 = prev1;
+//         prev1 = curr;
+//     }
+// return prev1;
 
 }
     int rob(vector<int>& nums) {
