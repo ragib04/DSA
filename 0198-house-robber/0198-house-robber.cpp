099bbf1ca42b237d8c8cabd4b7a99ccd79885bd9
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-07 14:12:30 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,20 +13,23 @@ int robed(int i, vector<int> &nums, vector<int>&dp){
 
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int> dp(n+1, -1);
-        return robed(n-1, nums, dp);
+        // vector<int> dp(n+1, -1);
+        // return robed(n-1, nums, dp);
 //Tabulation
-        // vector<int> dp(n+1);
-        // dp[0] = nums[0];
-        
-        // for(int i= 1; i<n; i++){
-        //     int pick = nums[i];
-        //     if(i>1) pick = pick+dp[i-2];
-        //     int not_pick = dp[i-1];
 
-        //     dp[i] = max(pick, not_pick);
-        // }
-        // return dp[n-1];
+vector<int> dp(n+1, 0);
+dp[0] = nums[0];
+ for(int i = 1; i<n; i++){
+    int pick = nums[i];
+    if(i>1) pick += dp[i-2];
+    int not_pick = dp[i-1];
+
+    dp[i] = max(pick, not_pick);
+
+
+ }
+ return dp[n-1];
+       
         
     }
 };
