5dac20e4eeac81bfea55260936488ccd1c632895
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-07 14:03:44 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-int robed(int ind, vector<int> &nums, vector<int> &dp){
-    if(ind == 0) return nums[ind];
-    if(ind<0) return 0;
 
-    if(dp[ind] != -1) return dp[ind];
+int robed(int i, vector<int> &nums, vector<int>&dp){
+    if(i == 0) return nums[i];
+    if(i<0) return 0;
+    if(dp[i] != -1) return dp[i];
+    int pick = nums[i]+ robed(i-2, nums, dp);
+    int not_pick = robed(i-1, nums, dp);
 
-    int pick = nums[ind] + robed(ind-2, nums, dp);
-    int not_pick = robed(ind-1, nums, dp);
-
-    dp[ind] = max(pick, not_pick);
-    return dp[ind];
+    return dp[i] = max(pick, not_pick);
 }
+
     int rob(vector<int>& nums) {
         int n= nums.size();
-        //vector<int> dp(n+1, -1);
-        vector<int> dp(n+1);
-        //return robed(n-1, nums, dp);
-        dp[0] = nums[0];
+        vector<int> dp(n+1, -1);
+        return robed(n-1, nums, dp);
+//Tabulation
+        // vector<int> dp(n+1);
+        // dp[0] = nums[0];
         
-        for(int i= 1; i<n; i++){
-            int pick = nums[i];
-            if(i>1) pick = pick+dp[i-2];
-            int not_pick = dp[i-1];
+        // for(int i= 1; i<n; i++){
+        //     int pick = nums[i];
+        //     if(i>1) pick = pick+dp[i-2];
+        //     int not_pick = dp[i-1];
 
-            dp[i] = max(pick, not_pick);
-        }
-        return dp[n-1];
+        //     dp[i] = max(pick, not_pick);
+        // }
+        // return dp[n-1];
+        
     }
 };
