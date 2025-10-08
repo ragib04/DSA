91875832ecc31ea79f91d27dd335dc14190a641b
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-08 13:13:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (83.63%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int solve(int i, string &s, int &n, vector<int> &dp){
+    if(i == n) return 1;
+    if(s[i] == '0') return 0;
+    if(dp[i] != -1) return dp[i];
+    int pick = solve(i+1, s, n, dp);
+    
+    if(i<n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1]<='6'))){
+        pick += solve(i+2, s, n, dp);
+
+    }
+
+    return dp[i] = pick;
+
+}
+    int numDecodings(string s) {
+        int n = s.size();
+        if(n == 0) return 0;
+        vector<int> dp(n+1, -1);
+
+        
+        return solve(0, s, n, dp);
+    }
+};
