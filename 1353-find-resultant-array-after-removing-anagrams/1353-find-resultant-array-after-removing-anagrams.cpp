5bbe16d1b5088148fc07ef3fc5bb1317ebdc5133
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-13 12:38:47 +0530

    Time: 11 ms (11.27%), Space: 21.1 MB (8.53%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+bool isanagrams(string st1, string st2) {
+        if (st1.size() != st2.size()) return false;
+        
+        unordered_map<char, int> mp;
+        for (char c : st1) mp[c]++;
+        for (char c : st2) {
+            if (--mp[c] < 0) return false;
+        }
+        return true;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> ans;
+        if(words.size() <=1) return words;
+        ans.push_back(words[0]);
+       for(int i = 1; i<words.size(); i++){
+            if(!isanagrams(words[i], words[i-1])) ans.push_back(words[i]);
+       } 
+       return ans;
+    }
+};
