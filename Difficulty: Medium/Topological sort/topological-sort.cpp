6230ca5fb3cd261a6130f0e07b104afe3cae9923
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-16 02:17:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,37 +1,35 @@
 class Solution {
   public:
-    vector<int> topoSort(int v, vector<vector<int>>& edges) {
+  
+  void topodfs(int node, vector<bool> &vis, stack<int> &st, unordered_map<int, list<int>> &adj){
+      vis[node] = 1;
+      for(auto neigh: adj[node]){
+          if(!vis[neigh]){
+              topodfs(neigh, vis, st, adj);
+          }
+          
+      }
+      st.push(node);
+  }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
-        unordered_map<int, list<int>> adj;
-        for(int i= 0; i<edges.size(); i++){
-            int x = edges[i][0];
-            int y = edges[i][1];
-            adj[x].push_back(y);
+        unordered_map<int, list<int>>adj;
+        for(int i = 0; i<edges.size(); i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
         }
-        // find all indegree
-        vector<int> indegree(v);
-        for(auto i: adj){
-            for(auto j: i.second){
-                indegree[j]++;
+        
+        vector<bool> vis(V, false);
+        stack<int> st;
+        for(int i = 0; i<V; i++){
+            if(!vis[i]){
+                topodfs(i, vis,st, adj);
             }
-        }
-        queue<int> q;
-        //push all 0 indegree in the queue
-        for(int i = 0; i<v; i++){
-            if(indegree[i] == 0) q.push(i);
-        }
-        // do bfs
-        vector<int> ans;
-        while(!q.empty()){
-            int front = q.front();
-            q.pop();
             
-            ans.push_back(front);
-            //neighbour indegree update
-            for(auto node: adj[front]){
-                indegree[node]--;
-                if(indegree[node] == 0) q.push(node);
-            }
+        }
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
         }
         return ans;
     }
