91581bc724e8f0759cbdd76598b23bd7cfd43fec
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-23 15:45:37 +0530

    Time: 3 ms (87.54%), Space: 8.4 MB (91.08%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
-
-        while (s.size() > 2) {
+        int n = s.size();
+        while (n > 2) {
             for (int i = 1; i < s.size(); i++) {
                 int sum = (s[i] - '0' + s[i - 1] - '0') % 10;
                 //s[i - 1] = to_string(sum);
                 s[i - 1] = char('0' + sum);
             }
             // dec the s size by 1
-            s.pop_back();
+            n--;
         }
 
         return s[0] == s[1];
