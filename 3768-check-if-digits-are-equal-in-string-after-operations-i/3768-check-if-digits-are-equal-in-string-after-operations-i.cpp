35b70eafb5bd8651396523efeafaca69c27e796d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-23 15:47:05 +0530

    Time: 2 ms (89.06%), Space: 8.4 MB (91.08%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -4,9 +4,9 @@ public:
         int n = s.size();
         while (n > 2) {
             for (int i = 1; i < s.size(); i++) {
-                int sum = (s[i] - '0' + s[i - 1] - '0') % 10;
+                // int sum = (s[i] - '0' + s[i - 1] - '0') % 10;
                 //s[i - 1] = to_string(sum);
-                s[i - 1] = char('0' + sum);
+                s[i - 1] = ((s[i] - '0' + s[i - 1] - '0') % 10) - '0';
             }
             // dec the s size by 1
             n--;
