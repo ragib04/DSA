b25549ecb3ee2ce113c447cd28254699d0616e0a
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-24 17:53:17 +0530

    Time: 0 ms (100%), Space: 8 MB (97.49%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+vector<int> freq{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int backtrack(int n , int curr, int cnt){
+    if(cnt == 0){
+        for(int digit = 1; digit<=9; digit++){
+            if(freq[digit] != 0 && freq[digit] != digit) return 0;
+        }
+        return curr>n ? curr: 0;
+    }
+    int res = 0;
+    for(int d = 1; d<=9; d++){
+        if(freq[d] != 0 && freq[d]<=cnt){
+            freq[d]--;
+            res = backtrack(n, curr*10+d, cnt-1);
+            freq[d]++;
+        }
+        if(res != 0) break;
+    }
+    return res;
+}
+    int nextBeautifulNumber(int n) {
+        int numDigit = to_string(n).length();
+
+        int result = backtrack(n, 0, numDigit);
+        if(result == 0){
+            result = backtrack(n, 0, numDigit+1);
+        }
+        return result;
+    }
+};
+
+/*
+class Solution {
+public:
 bool balanced(int num){
     vector<int> freq(10);
     while(num>0){
@@ -20,3 +53,4 @@ bool balanced(int num){
         return 0;
     }
 };
+*/
