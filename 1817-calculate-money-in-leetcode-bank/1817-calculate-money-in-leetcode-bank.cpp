db2028b57bde8d3b04c3e3592ea56f6345b26d99
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-25 12:09:21 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.15%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int monday  = 1;
+        int monday = 0;
+        int cnt = 1;
         int total = 0;
-        int i = 1;
-        int day = 0;
-        while(n != 0){
-            day++;// 1 2 3 4  5 6 7 0 1 2
-            total += i; // 1 3 6 10 15 21 28 31 34 28
-            i++; // 2 3 4 5 6 7 3 4
-            n--;// 9 8 7 6 5 4 3 2 1
-            
-            if(day == 7){
-                i = monday+1;
-                monday += 1;
-                day = 0;
-            }
-
+        for(int i = 1; i<=n; i++){
+            total += cnt;
+            cnt++;
+            if(cnt > 7+monday){
+                 monday += 1;
+                 cnt = 1+monday;
+                }         
         }
         return total;
     }
+
 };
