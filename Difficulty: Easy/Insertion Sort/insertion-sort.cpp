2266e30499bf37521712a2910eb51cdb7f5a6d89
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-10-31 17:28:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -1,17 +1,25 @@
 class Solution {
   public:
+  void insertionsortrec(vector<int> &arr, int n){
+      if(n<=1) return;
+      
+      insertionsortrec(arr, n-1);
+      
+      int last = arr[n-1];
+      int j = n-2;
+      
+      while(j>=0 && arr[j]>last){
+          arr[j+1] = arr[j];
+          j--;
+      }
+      
+      arr[j+1] = last;
+      
+  }
     void insertionSort(vector<int>& arr) {
         // code here
         int n = arr.size();
-        for(int i = 1; i<n; i++){
-            int key = arr[i]; 
-            
-            int j = i-1;
-            while(j>=0 && arr[j]>key){ //[1 3 4 7 9]
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = key;
-        }
+       insertionsortrec(arr, n);
+        
     }
 };
