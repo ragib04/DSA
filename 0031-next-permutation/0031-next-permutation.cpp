2d9a0faeb7be546683b3100ea85f244ec56bd5e6
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-11-04 15:38:25 +0530

    Time: 8 ms (1.32%), Space: 15.7 MB (32.49%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -9,10 +9,10 @@ public:
                 break;
             }
         }
-        if(ind == -1){
-            reverse(nums.begin(), nums.end());
-            return;
-        }
+       if(ind == -1){
+        reverse(nums.begin(), nums.end());
+        return ;
+       }
         for(int i = n-1; i>=0; i--){
             if(nums[i]>nums[ind]){
                 swap(nums[i], nums[ind]);
@@ -20,5 +20,6 @@ public:
             }
         }
         reverse(nums.begin()+ind+1, nums.end());
+        
     }
 };
