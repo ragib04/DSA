ce656afb18f6967b609943c16f18fda274222ab9
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-11-11 02:19:57 +0530

    Time: 59 ms (38.99%), Space: 222.2 MB (53.05%) - LeetHub

--- 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
+        stack<int> st;
+        int ops = 0;
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && st.top()>nums[i]){
+                st.pop();
+            }
+            if(nums[i] == 0) continue;
+            if(st.empty() || st.top()<nums[i]){
+                st.push(nums[i]);
+                ops++;
+                
+            }
+        }
+        return ops;
+    }
+};
+
+/*
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
         vector<int> stack(n+1, 0);
         int top = 0, ans = 0;
         for(int num: nums){
@@ -17,7 +39,5 @@ public:
         return ans+top;
     }
 
-
-
-    
 };
+*/
