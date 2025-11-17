ccc83f1ea2b9e813859d03f5f852fff0e0f70396
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-11-17 19:42:18 +0530

    Time: 0 ms (100%), Space: 61.4 MB (47.64%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int cnt = 0;
-        bool flag = false;
-      for(int i = 0; i<nums.size(); i++){
+    //     int cnt = 0;
+    //     bool flag = false;
+    //   for(int i = 0; i<nums.size(); i++){
+    //     if(nums[i] == 1){
+    //         if(cnt<k && flag == true) return false;
+    //         cnt = 0;
+    //         flag = true;
+    //     }
+    //     else {
+    //         cnt++;  
+    //     }
+    //   }
+    //   return true; 
+    int prev_ind = -1e9;
+    for(int i = 0; i<nums.size(); i++){
         if(nums[i] == 1){
-            if(cnt<k && flag == true) return false;
-            cnt = 0;
-            flag = true;
+            if(i-prev_ind-1<k) return false;
+            prev_ind = i;
         }
-        else {
-            cnt++;  
-        }
-      }
-      return true; 
+    }
+    return true;
     }
 };
