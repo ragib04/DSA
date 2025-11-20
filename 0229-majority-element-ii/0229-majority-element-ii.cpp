0bd6b34fa6cb6b8ff7e10ea1e4e0194a02d506fe
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-11-20 19:22:21 +0530

    Time: 8 ms (31.63%), Space: 26.2 MB (37.7%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,15 +3,17 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
         unordered_map<int, int> mp;
-        for(auto i: nums){
-            mp[i]++;
+        for(auto it: nums){
+            mp[it]++;
+            
         }
-        for(auto it: mp){
-            if(it.second>(nums.size()/3))
-            {
-                ans.push_back(it.first);
+        int n = nums.size();
+        for(auto i: mp){
+            if(i.second > n/3){
+                ans.push_back(i.first);
             }
         }
         return ans;
+
     }
 };
