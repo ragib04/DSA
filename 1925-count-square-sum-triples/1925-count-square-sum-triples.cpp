4353379ebab63b922a5e0d2e009f3aed06e28c5d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-12-08 12:11:25 +0530

    Time: 11 ms (44.91%), Space: 9 MB (8.85%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     int countTriples(int n) {
         int cnt = 0;
+       unordered_set<int> st;
        for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=n; j++){
-            for(int k = 1;k<=n; k++){
-                if(sqrt((i*i) + (j*j)) == k) cnt++;
+            st.insert(i*i);
+       }
+       for(int i = 1; i<=n; i++){
+        int temp = i*i;
+        for(int j = i+1; j<=n; j++){
+            int temp2 = temp+ j*j;
+            if(st.count(temp2)){
+                cnt += 2;
             }
         }
        }
