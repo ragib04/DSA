a1fd5293559f24dbcbc3b282e9e76903d95fa20d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-12-16 13:36:32 +0530

    Time: 0 ms (100%), Space: 29.6 MB (61.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,11 +3,16 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<int> temp(n);
-        // temp[(i+k)%n] = nums[i];
-        for(int i = 0; i<n;i++){
-            temp[(i+k)%n] = nums[i];
-        }
-        nums = temp;
+        // vector<int> temp(n);
+        // // temp[(i+k)%n] = nums[i];
+        // for(int i = 0; i<n;i++){
+        //     temp[(i+k)%n] = nums[i];
+        // }
+        // nums = temp;
+        // [1,2,3,4,5,6,7]
+        k  = k%n;
+        reverse(nums.begin(), nums.end()); // [7,6,5,4,3,2,1]
+        reverse(nums.begin(), nums.begin()+k);//[5,6,7,4,3,2,1]
+        reverse(nums.begin()+k, nums.end()); // [5,6,7,1,2,3,4]
     }
 };
