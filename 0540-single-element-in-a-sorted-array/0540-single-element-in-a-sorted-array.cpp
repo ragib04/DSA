cc68f44aacbb2935b12876ce7efc0cb1816a954c
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2025-12-30 13:55:56 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.32%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,22 +4,25 @@ public:
         int n = nums.size();
         int s = 0, e = n-1;
         if(n == 1) return nums[0];
-        //bpundary condition
-        if(nums[s] != nums[s+1]) return nums[s];
+         if(nums[s] != nums[s+1]) return nums[s];
         if(nums[e] != nums[e-1]) return nums[e];
+        
         while(s<=e){
-
-            int mid = (s+e)/2;
-            if((nums[mid] != nums[mid+1]) && (nums[mid] != nums[mid-1])){
-                return nums[mid];
-
-            }
+            int mid = s + (e-s)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            
             if((mid%2 == 1 && nums[mid] == nums[mid-1]) || (mid%2 == 0 && nums[mid] == nums[mid+1])){
                 s = mid+1;
             }
-            else{ e = mid-1;}
+             else{
+               
+                    e = mid-1;
+                
+            }
 
-        }
+            }
+           
+        
         return 0;
     }
 };
