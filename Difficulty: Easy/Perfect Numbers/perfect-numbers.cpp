a90bb3cc893f15eb9bab549682cfe93dceb8e6bf
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-01-08 22:29:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool isPerfect(int n) {
+        // code here
+        int sum = 1;
+        for(int i = 2; i*i<=n; i++){
+            if(n%i == 0){
+                if(i*i != n){
+                    sum += i+ n/i;
+                }
+                else sum += i;
+            }
+        }
+        if(sum == n && n != 1) return true;
+        return false;
+        
+    }
+};
