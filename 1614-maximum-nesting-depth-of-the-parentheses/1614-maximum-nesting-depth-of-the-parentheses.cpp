ed0a7b87f595a176de2c81a0d33a9c22f929ef5a
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-01-14 13:15:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (84.27%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n= s.size();
+        int open = 0;
+        int cnt = 0;
+        int maxi = 0;
+        for(int i = 0; i<n; i++){
+            if(s[i] == '('){
+                open++;
+            }
+            else if(s[i] == ')'){
+                
+                maxi = max(maxi, open);
+                open--;
+                }
+            }
+            
+        
+        return maxi;
+    }
+};
