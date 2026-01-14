e7983b9e7b362583d776ea2579c6d8fc9d5b2a7b
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-01-15 00:18:05 +0530

    Time: 0 ms (100%), Space: 10 MB (79.86%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-
-
+        //step 1
         reverse(s.begin(), s.end());
-        int l = 0,r = 0, i = 0;
+        int i = 0, r= 0, l = 0;
         int n = s.size();
         while(i<n){
             while(i<n && s[i] == ' '){
                 i++;
-                
             }
             if(i == n) break;
             while(i<n && s[i] != ' '){
                 s[r++] = s[i++];
+                
             }
             reverse(s.begin()+l, s.begin()+r);
             s[r++] = ' ';
-            l= r;
+            
+            l = r;
             i++;
-
         }
         s.resize(r-1);
-    
-
         return s;
-
     }
 };
