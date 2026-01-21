94b43b06dc9991a1b8356a17a5e720a55fc98680
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-01-21 13:55:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (80.5%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
 int helper(int i, string &s, long long num, int sign, int n){
-    if(i>=n || !isdigit(s[i])){
+    if(i>=n || !(s[i]>='0' && s[i]<='9')){
         return (int)(sign*num);
     }
     int digit = s[i] - '0';
@@ -26,3 +26,31 @@ int helper(int i, string &s, long long num, int sign, int n){
         return helper(i, s, 0, sign, n);
     }
 };
+
+
+/*
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        long long num = 0;
+        int i = 0;
+        int sign = 1;
+        while(i<n && s[i] == ' ') i++;
+
+        if(i<n && s[i] == '+' || s[i] == '-'){
+            sign = (s[i] == '+')? 1: -1;
+            i++;
+        }
+        if(s[i] == '0') i++;
+        while(i<n && s[i]>='0' && s[i]<='9'){
+            int digit = s[i] - '0';
+            if(num > (INT_MAX-digit)/10) return sign == 1 ? INT_MAX: INT_MIN;
+
+            num = num*10+digit;
+            i++;
+        }
+        return sign * (int)num;
+    }
+};
+*/
