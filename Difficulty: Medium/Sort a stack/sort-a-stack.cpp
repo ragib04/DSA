d4ea12b10487faf2d903f82d1c483bdcbf1018af
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-01-24 12:02:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,22 +1,21 @@
 class Solution {
   public:
-  void stinsert(stack<int> &st, int ele){
-      if(st.empty() || st.top()<=ele){
-          st.push(ele);
-          return;
-      }
-      int val = st.top();
-      st.pop();
-      stinsert(st, ele);
-      st.push(val);
-      return;
-  }
+ void insert(stack<int> &st, int ele){
+     if(st.empty() || st.top()<ele){
+         st.push(ele);
+         return;
+     }
+     int top= st.top();
+     st.pop();
+     insert(st, ele);
+     st.push(top);
+ }
     void sortStack(stack<int> &st) {
         // code here
-        if(st.empty() || st.size() == 1) return;
-        int top = st.top();
-        st.pop();
-        sortStack(st);
-        stinsert(st, top);
+       if(st.empty() || st.size() == 1) return;
+       int top = st.top();
+       st.pop();
+       sortStack(st);
+       insert(st, top);
     }
 };
