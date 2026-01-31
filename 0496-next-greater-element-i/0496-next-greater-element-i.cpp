0f86a7829b5e4dc28ccabab7586cb12c96b513c0
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-01-31 15:52:51 +0530

    Time: 0 ms (100%), Space: 12.7 MB (58.63%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,26 +3,25 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
         vector<int> ans(n, -1);
-        vector<int> NGE(m, -1);
+        
+        unordered_map<int, int>NGE;
         stack<int> st;
         for(int i = m-1;i>=0; i--){
             while(!st.empty()){
                 if(st.top()>nums2[i]){
-                    NGE[i] = st.top();
+                    NGE[nums2[i]] = st.top();
                     break;
                 }
                 st.pop();
-
             }
             st.push(nums2[i]);
+            
         }
         for(int i = 0; i<n; i++){
-            int ele = nums1[i];
-            for(int j = 0; j<m; j++){
-                if(nums2[j] == ele){
-                    ans[i] = NGE[j];
-                }
+            if(NGE.find(nums1[i]) != NGE.end()){
+                ans[i] = NGE[nums1[i]];
             }
+            else ans[i] = -1;
         }
         return ans;
     }
