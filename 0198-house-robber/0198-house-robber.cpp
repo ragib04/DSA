ca517c161c6318b16b15e0b0eff62722d6069fb5
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-03-13 23:29:55 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,7 +12,17 @@ int solve(int ind, vector<int> &nums, vector<int> &dp){
 }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, -1);
-        return solve(n-1, nums, dp);
+        //vector<int>dp(n+1, -1);
+       // return solve(n-1, nums, dp);
+       vector<int> dp(n, 0);
+       dp[0] = nums[0];
+       for(int i = 1; i<n; i++){
+        int take = nums[i];
+        if(i>1) take += dp[i-2];
+        int not_take = dp[i-1];
+        dp[i] = max(take, not_take);
+       }
+       return dp[n-1];
+
     }
 };
