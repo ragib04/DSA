a5d0d1d7a2158a03718d91a0fbcd1363b9608181
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-04-09 13:25:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,44 +1,37 @@
 class Solution {
   public:
-  void merge(vector<int>& arr,int l,int mid, int r){
-      vector<int>temp;
-      int start=l,end=mid+1;
-      while(start<=mid && end<=r){
-          if(arr[start]<=arr[end]){
-              temp.push_back(arr[start]);
-              start++;
+  void merge(vector<int> &arr, int l, int r, int mid){
+      vector<int> temp;
+      
+      int s = l, e = mid+1;
+      while(s<=mid && e<=r){
+          if(arr[s]<=arr[e]){
+              temp.push_back(arr[s]);
+              s++;
           }
           else{
-              temp.push_back(arr[end]);
-              end++;
+              temp.push_back(arr[e]);
+              e++;
           }
-          
-        }
-        while(start<=mid){
-            temp.push_back(arr[start]);
-            start++;
-        }
-        while(end<=r){
-            temp.push_back(arr[end]);
-            end++;
-        }
-        for(int i=l;i<=r;i++){
-            arr[i]=temp[i-l];
-        }
-  
-      
-  }
-  void ms(vector<int>& arr, int l, int r){
-      if(l>=r)
-      return;
-      int mid=(l+r)/2;
-      ms(arr,l,mid);
-      ms(arr,mid+1,r);
-      merge(arr,l,mid,r);
+      }
+      while(s<=mid){
+          temp.push_back(arr[s]);
+          s++;
+      }
+      while(e<=r){
+          temp.push_back(arr[e]);
+          e++;
+      }
+      for(int i = l; i<=r; i++){
+          arr[i] = temp[i-l];
+      }
   }
     void mergeSort(vector<int>& arr, int l, int r) {
-        int n=arr.size();
-        ms(arr,0,n-1);
-       
+        // code here
+        if(l>=r) return;
+        int mid = (l+r)/2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1,r);
+        merge(arr, l, r, mid);
     }
 };
