06851c86c6085852a69b5b41965abca20161ed30
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-04-13 12:57:19 +0530

    Time: 6 ms (18.77%), Space: 24 MB (7.69%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,9 +2,17 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(int i = 0; i<nums.size(); i++){
-            ans ^= nums[i];
+        unordered_set<int> st;
+        int sum = 0;
+        for(auto it: nums){
+            sum += it;
+            st.insert(it);
         }
-        return ans;
+        int sum_1 = 0;
+        for(auto it: st){
+            sum_1 += it;
+        }
+        return 2*sum_1 - sum;
     }
 };
+//14
