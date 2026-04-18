9f5a064aa3087910eab6c65463a7bf6880d2dd99
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-04-18 13:00:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (80.74%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int count  = 0;
+        int n = matrix.size(), m = matrix[0].size();
         int total = n*m;
-
-        int startingRow = 0, startingCol = 0;
-        int endingRow = n-1, endingCol = m-1;
+        int cnt = 0;
+        int s_r = 0, s_c = 0;
+        int e_r = n-1, e_c = m-1;
         vector<int> ans;
-        while(count<total){
-            for(int i = startingCol; i<=endingCol && count<total; i++){
-                ans.push_back(matrix[startingRow][i]);
-                count++;
-            }
-            startingRow++;
-            for(int i = startingRow; i<=endingRow && count<total; i++){
-                ans.push_back(matrix[i][endingCol]);
-                count++;
-            }
-            endingCol--;
-            for(int i = endingCol; i>=startingCol && count<total; i--){
-                ans.push_back(matrix[endingRow][i]);
-                count++;
-            }
-            endingRow--;
-            for(int i = endingRow; i>=startingRow && count<total; i--){
-                ans.push_back(matrix[i][startingCol]);
-                count++;
-            }
-            startingCol++;
+        while(cnt<total){
+        for(int i = s_r; i<=e_c && cnt<total; i++){
+            ans.push_back(matrix[s_r][i]);
+            cnt++;
         }
-        return ans;
-        
+        s_r++;
+        for(int i = s_r; i<=e_r && cnt<total; i++){
+            ans.push_back(matrix[i][e_c]);
+            cnt++;
+        }
+        e_c--;
+        for(int i = e_c; i>=s_c && cnt<total; i--){
+            ans.push_back(matrix[e_r][i]);
+            cnt++;
+        }
+        e_r--;
+        for(int i = e_r; i>=s_r && cnt<total; i--){
+            ans.push_back(matrix[i][s_c]);
+            cnt++;
+
+        }
+        s_c++;
+    }
+    return ans;
     }
 };
