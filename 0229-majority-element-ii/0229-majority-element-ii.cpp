42b3686f9c472acbef3927abebd2457c761eed3d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-04-20 13:14:44 +0530

    Time: 29 ms (5.02%), Space: 26.3 MB (36.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,12 +5,10 @@ public:
         for(auto it: nums){
             mp[it]++;
         }
-        int n = nums.size();
         vector<int> ans;
-        for(auto num: mp){
-            if(num.second > n/3){
-                ans.push_back(num.first);
-            }
+        int n = nums.size();
+        for(auto it: mp){
+            if(it.second>n/3) ans.push_back(it.first);
         }
         return ans;
     }
