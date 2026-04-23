251552eb50ad266fdb9e56425978e7e3fbdcd82d
MD RAGIB IMRAN <137068732+ragib04@users.noreply.github.com>
2026-04-23 12:00:56 +0530

    Time: 0 ms (100%), Space: 17.7 MB (85.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-
 /*
-The key to solving this problem is understanding how negative numbers and zero affect the product of a subarray. A single negative number can turn the smallest product into the largest if multiplied correctly, so we must track both the maximum and minimum products at each step.
+
 */
     int maxProduct(vector<int>& nums) {
-        
-int n = nums.size();
-int currmax = nums[0];
-int currmin = nums[0];
-int ans = nums[0];
+        int prefix = 1, suffix = 1;
+        int n = nums.size();
+        int maxi = INT_MIN;
 
-        for(int i = 1; i<n; i++){
-           if(nums[i] <0) swap(currmax, currmin);
-
-           currmax= max(nums[i], currmax*nums[i]);
-           currmin = min(nums[i], currmin*nums[i]);
-
-           ans = max(currmax, ans);
-            
-            
+        for(int i = 0;i<n; i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+    maxi = max(maxi, max(prefix, suffix));
         }
-        return ans;
+        return maxi;
     }
 };
